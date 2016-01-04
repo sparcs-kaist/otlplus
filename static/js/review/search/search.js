0eb24ab2583ecce3ffc4569eb256ad4caec864f6
Kang-in Raon Kim <talkwithraon@gmail.com>
2016-01-04 23:23:59 +0900

    폰트 어썸 추가, 사소한 부분 제외하고 완료

--- static/js/review/search/search.js
+++ static/js/review/search/search.js
@@ -1,3 +1,12 @@
-$('#category').on('focus', function() {
-    $('#m-category').modal('show');
+$("#option").on("click", function() {
+    $("#options").toggleClass("active");
+});
+
+$("#options input[type='checkbox']").on("change", function() {
+    var target = $(event.target);
+
+    if(!target.is("label"))
+        target = target.closest('label');
+
+    target.toggleClass("active");
 });
