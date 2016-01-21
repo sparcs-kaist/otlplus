7e71c45a48efacdc5a177f1b67b2b3624f251a26
jiggum <0s.dongmin@gmail.com>
2016-01-22 00:47:03 +0900

    Let admin-site fater

--- apps/subject/admin.py
+++ apps/subject/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from apps.subject.models import *
 # Register your models here.
-
-admin.site.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    raw_id_fields = 'course',
+ 
+admin.site.register(Lecture, LectureAdmin)
 admin.site.register(Department)
 admin.site.register(Course)
 admin.site.register(Professor)
