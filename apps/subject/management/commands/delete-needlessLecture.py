5708fda317e3fb31c5237d531aa50a364b6922f1
jiggum <0s.dongmin@gmail.com>
2016-01-22 00:44:00 +0900

    add deleting unnecessary lectures, upadating professors' courselist scripts

---
+++ apps/subject/management/commands/delete-needlessLecture.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand
+from apps.subject.models import Lecture
+from django.db.models import Q
+class Command(BaseCommand):
+    help = 'delete unnecessary lecture'
+    def handle(self, *args, **options):
+        print "delete unnecessary lecture start!"
+        count = 0
+        for aLecture in list(Lecture.objects.filter(credit=0, num_classes=0, num_labs=0, credit_au=0)):
+            count+=1
+            print aLecture.title, aLecture.id, aLecture.credit, aLecture.num_classes, aLecture.num_labs, aLecture.credit_au
+            aLecture.delete()
+
+        for aLecture in list(Lecture.objects.filter(Q(title__startswith=u"개별연구")| Q(title__startswith=u"졸업연구") | Q(title__startswith=u"논문연구"))):
+            count+=1
+            print aLecture.title, aLecture.id, aLecture.credit, aLecture.num_classes, aLecture.num_labs, aLecture.credit_au
+            aLecture.delete()
+         
+        for aLecture in list(Lecture.objects.filter(Q(type__startswith=u"개별연구")| Q(type__startswith=u"졸업연구") | Q(type__startswith=u"논문연구"))):
+            count+=1
+            print aLecture.title, aLecture.id, aLecture.credit, aLecture.num_classes, aLecture.num_labs, aLecture.credit_au
+            aLecture.delete()
+
+        for aLecture in list(Lecture.objects.filter(Q(type__startswith=u"연구필수"))):
+            count+=1
+            print aLecture.title, aLecture.id, aLecture.credit, aLecture.num_classes, aLecture.num_labs, aLecture.credit_au
+            aLecture.delete()
+        print "total %d Lectures deleted!"%count
---
+++ apps/subject/management/commands/update_ProfessorCourseList.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from apps.subject.models import Course, Professor
+class Command(BaseCommand):
+    help = "update Professors' course_list"
+    def handle(self, *args, **options):
+        print "start updating Professors' course_list"
+        for aCourse in list(Course.objects.all()):
+            for aProfessor in list(aCourse.professors.all()):
+                aProfessor.course_list.add(aCourse)
+                print "%s %s"%(aCourse, aProfessor)
+        print "Professors' course_list changed"
+    
