ba6c8690d9c94621c4ed722c6efc7111b93ef2a7
jihoon-ko <mango@bit>
2016-02-03 01:15:50 +0900

    후기작성페이지 views.py 간소화 및 프론트엔드 대응을 위한 학기 구분 추가

--- apps/review/views.py
+++ apps/review/views.py
@@ -253,19 +253,10 @@ def ReviewInsertView(request,lecture_id=-1,semester=0):
     pre_grade="A"
     pre_load="A"
     pre_speech="A"
-    guideline="1) 학점, 로드 등의 평가에 대하여 왜 그렇게 평가를 했는지 간단히 서술해주세요.\n"
-    guideline=guideline+"진도를 빠르고 많이 나가는 경향이 있습니다 / 학점은 절대평가로 주셔서 열심히 공부하면 잘 나오는 것 같아요 / 과제는 6번 주어졌고, 각 과제마다 시간이 약 6 ~ 8 시간 소요된 것 같습니다.\n"
-    guideline=guideline+"2) 최대한 본인의 감정을 배제해주시면 감사하겠습니다.\n"
-    guideline=guideline+"ex) 교수님이 저를 맘에 안 들어하시는 것 같아요 (X) / 시험기간에 플젝 내준건 좀 너무하다싶었음 (X)\n"
-    guideline=guideline+"3) 나중에 이 수업을 들을 학생들을 위해 여러 조언들을 해 주세요.\n"
-    guideline=guideline+"ex) 이 수업을 들으시려면 먼저 이산구조와 데이타구조를 먼저 듣고 오시는 것을 추천합니다. / 교수님이 수업하실 때 열심히 참여하시면 좋을 것 같아요.\n"
-    print lecture_id, semester
-    print lecture_id == -1
-    print semester > 0
+    guideline="".join(open("apps/review/guideline","r").readlines())
     if str(lecture_id)==str(-1) and semester > 0:
         return HttpResponseRedirect('../../' + str(lecture_list[0].id) + '/'+str(return_object[0]["lectime"]))
     if semester > 0:
-        print lecture_id
         now_lecture = user.take_lecture_list.get(id=lecture_id,year=lec_year,semester=lec_sem)
         try :
             temp = user.comment_set.all()
