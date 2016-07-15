818a18e3f8fda5b70a9e66a1e9d99479efa4ec07
JONG HUN <ojh3636@kaist.ac.kr>
2016-07-15 17:24:12 +0900

    Add ClassTime class and ExamTIme class
    Add ClassTimeAdmin and ExamTimeAdmin for admin page binding

--- apps/subject/admin.py
+++ apps/subject/admin.py
@@ -5,8 +5,20 @@ class LectureAdmin(admin.ModelAdmin):
     raw_id_fields = 'course',
 class CourseFilteredAdmin(admin.ModelAdmin):
     raw_id_fields = 'courses',
+
+class ExamTimeAdmin(admin.ModelAdmin):
+	list_display = ('lecture', 'day', 'begin', 'end')
+	ordering = ('lectur',)
+
+class ClassTimeAdmin(admin.ModelAdmin):
+	list_display = ('lecture', 'day', 'begin', 'end', 'roomNum', 'roomName_ko', 'roomName_en')
+	ordering('lecture',)
+
+
 admin.site.register(Lecture, LectureAdmin)
 admin.site.register(Department)
 admin.site.register(Course)
 admin.site.register(Professor)
 admin.site.register(CourseFiltered,CourseFilteredAdmin)
+admin.site.register(ExamTime,ExamTimeAdmin)
+admin.site.register(ClassTime,ClassTimeAdmin)
