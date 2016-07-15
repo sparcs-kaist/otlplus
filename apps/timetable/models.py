38aa930041ea517667ce3bff217e9a6a908ef758
JONG HUN <ojh3636@kaist.ac.kr>
2016-07-15 18:04:05 +0900

    add TimeTable class in models.py and add TImeTableAdmin class in admin.py

--- apps/timetable/models.py
+++ apps/timetable/models.py
@@ -1,3 +1,15 @@
+# -*- codingd: utf-8
 from django.db import models
+from apps.subject.models import *
+
+class TimeTable(models.Model):
+	lecture = ForeignKey(Lecture)
+	year = IntegerField(null=True) # 몇넌도의 타임테이블인지
+	semester = SmallIntegerField(null=True) # 어떤학기의 타임테이블인지
+	table_id = SmallIntegerField(null=True) # 몇번째 타임테이블인지 0,1,2,3
+
+
 
 # Create your models here.
+
+
