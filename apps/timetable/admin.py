af04ac46d4174f7572ec6c084226bc7cf1250dc8
JONG HUN <ojh3636@kaist.ac.kr>
2016-07-15 18:04:05 +0900

    add TimeTable class in models.py and add TImeTableAdmin class in admin.py

--- apps/timetable/admin.py
+++ apps/timetable/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from apps.timetable.models import TimeTable
 
+class TimeTableAdmin(admin.ModelAdmin):
+	list_disply = ('year', 'semester', 'table_id')
+	ordering = ('year','semester','table_id',)
+
+admin.site.register(TimeTable, TimeTableAdmin)
+	
 # Register your models here.
