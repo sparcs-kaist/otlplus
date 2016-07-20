586750563173b318418b045a97483b20efbb5604
ashe-sparcs <wotjd1102@naver.com>
2016-07-20 10:16:15 -0400

    Light Year Kim Issue - email parser upgraded

--- apps/subject/management/commands/delete-duplicate-professor.py
+++ apps/subject/management/commands/delete-duplicate-professor.py
@@ -5,13 +5,11 @@ from django.core.exceptions import *
 from apps.subject.models import Department, Professor, Lecture, Course
 #from otl.apps.timetable.models import ClassTime, ExamTime, Syllabus
 from optparse import make_option
-from datetime import time
 import sys, getpass, re
 #import Sybase
-import pyodbc
-import datetime
 from apps.subject.models import *
 import urllib2
+import urllib
 
 class Command(BaseCommand):
     help = u'Delete and Merge duplicate professors - professors with same name, department, and email address'
@@ -22,10 +20,26 @@ class Command(BaseCommand):
         professors_names = map(lambda x: x.professor_name, list(professors))
         professors_duplicate = map(lambda x: professors.count(x) > 1, professors)
         '''
-        print self.get_email("2016", "3", "36.341", "%20%20", "4421")
+        print self.get_email(u'\uae40\uad11\ub144')
 
-    def get_email(self, year, term, subject_no, lecture_class, dept_id):
+    def get_lecture_class(self, class_no):
+        class_no = class_no.encode('utf-8')
+        if class_no == '':
+            return '%20%20'
+        else:
+            return class_no + '%20'
+
+    def get_email(self, prof_name):
+        lecture = Lecture.objects.filter(professor=Professor.objects.all().filter(professor_name=prof_name.encode('utf-8'))[0])[0]
+        year = str(lecture.year)
+        term = str(lecture.semester)
+        subject_no = lecture.code.encode('utf-8') 
+        lecture_class = self.get_lecture_class(lecture.class_no)
+        dept_id = str(lecture.department.id)
+
+        #param = urllib.urlencode({'year': str(lecture.year), 'term': str(lecture.semester), 'subject_no': lecture.code.encode('utf-8'), 'lecture_class': self.get_lecture_class(lecture.class_no), 'dept_id': str(lecture.department.id)})
         req = urllib2.Request("https://ssogw6.kaist.ac.kr/syllabusInfo?year="+year+"&term="+term+"&subject_no="+subject_no+"&lecture_class="+lecture_class+"&dept_id="+dept_id, headers={ 'User-Agent': 'Mozilla/5.0' })
+        #req = urllib2.Request("https://ssogw6.kaist.ac.kr/syllabusInfo?"+param, headers={ 'User-Agent': 'Mozilla/5.0' })
         html = urllib2.urlopen(req).read()
         email = html.split("<th>E-Mail</th>")[1].split("</td>")[0].split("<td>")[1]
         return email
