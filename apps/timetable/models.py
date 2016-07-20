5899ec849236133532d38a61232967b9cc5f4c44
JONG HUN <ojh3636@kaist.ac.kr>
2016-07-20 18:00:43 +0900

    Change lecture property ForeignKey to ManyToMany Field

--- apps/timetable/models.py
+++ apps/timetable/models.py
@@ -5,7 +5,7 @@ from apps.session.models import *
 
 
 class TimeTable(models.Model):
-	lecture = ForeignKey(Lecture)
+	lecture = models.ManyToManyField(Lecture)
 	user = models.ForeignKey(UserProfile, related_name="timetable_set")
 	year = IntegerField(null=True) # 몇넌도의 타임테이블인지
 	semester = SmallIntegerField(null=True) # 어떤학기의 타임테이블인지
