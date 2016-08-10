f820935f1b45bd3ce10b631cb50883548796fcea
jihoon-ko <jihoonko@kaist.ac.kr>
2016-08-10 17:43:38 +0900

    Fix issue #162 about script

--- db_update_script.sh
+++ db_update_script.sh
@@ -3,6 +3,11 @@ echo "Start!"
 echo $(date +'%Y-%m-%d / %T') > db_update_script.log
 
 python manage.py makemigrations >> db_update_script.log
+python manage.py makemigrations review >> db_update_script.log
+python manage.py makemigrations session >> db_update_script.log
+python manage.py makemigrations timetable >> db_update_script.log
+python manage.py makemigrations subject >> db_update_script.log
+python manage.py check >> db_update_script.log
 python manage.py migrate >> db_update_script.log
 python update_scholardb.py >> db_update_script.log
 python manage.py delete-needlessLecture >> db_update_script.log
