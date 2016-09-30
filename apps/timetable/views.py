91a25cd14c80e6b0939ef62b150ab076853ee579
Jeong Woon Choi <choijeongwoon@gmail.com>
2016-09-30 22:07:07 +0900

    timetable: implement print functions

--- apps/timetable/views.py
+++ apps/timetable/views.py
@@ -5,11 +5,14 @@ from apps.session.models import UserProfile
 
 # Django modules
 from django.core.exceptions import *
-from django.http import *
-from django.contrib.auth.decorators import login_required
-from utils.decorators import login_required_ajax
+from django.core.servers.basehttp import FileWrapper
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import *
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from otl.utils.decorators import login_required_ajax
+
 # For google calender
 from apiclient import discovery
 import oauth2client
@@ -21,6 +24,9 @@ import httplib2
 
 # Misc
 import os
+import pdfkit
+import tempfile
+
 def test(request):
     context = {'pagetTitle': 'JADE 사용', 'youAreUsingJade': True}
     return render(request, 'test.jade', context)
@@ -28,10 +34,10 @@ def test(request):
 def main(request):
     return render(request, 'timetable/index.html')
 
+
 @login_required_ajax
 def calendar(request):
-    """Exports otl timetable to google calender
-    """
+    """ Exports otl timetable to google calender """
     user = request.user
     try:
         userprofile = UserProfile.objects.get(user=user)
@@ -81,3 +87,72 @@ def calendar(request):
 
    # TODO: Add calendar entry
 
+def _make_timetable_html(request):
+   ''' From request make a html(string) used for printing or exporting '''
+   table_id = int(request.GET.get('id', 0))
+   view_year = int(request.GET.get('view_year', settings.NEXT_YEAR))
+   view_semester = int(request.GET.get('view_semester', settings.NEXT_SEMESTER))
+
+   # Load appropriate models
+
+   return render_to_string('timetable/print.jade', context=None,
+                           request=request)
+
+def _get_pdf(rendered_string):
+    html_num, html_path = tempfile.mkstemp(suffix='.html', dir='/tmp')
+    html_file = open(html_path, 'w')
+    html_file.write(rendered_string.encode('utf-8'))
+    html_file.close()
+
+    pdf_num, pdf_path = tempfile.mkstemp(suffix='.pdf', dir='/tmp')
+
+    cmd = 'prince -i=html --media=screen '
+
+    css_list = ['css/bootstrap.min.css', 'css/font-awesome.min.css',
+                'css/gobal.css', 'css/components/header/result.css', 'css/table.css',
+                'css/timetable/table.css', 'css/timetable/timetable.css']
+    for css in css_list:
+        cmd += '-s '
+        cmd += os.path.join(settings.STATICFILES_DIRS, css)
+        cmd += ' '
+
+    cmd += html_path
+    cmd += ' -o ' + pdf_path
+
+    os.system(cmd)
+
+    return pdf_path
+
+@login_required
+def save_as_pdf(request):
+   ''' Serve pdf file based on request and template '''
+   table_id = int(request.GET.get('id', 0))
+   view_year = int(request.GET.get('view_year', settings.NEXT_YEAR))
+   view_semester = int(request.GET.get('view_semester', settings.NEXT_SEMESTER))
+   old_lang = request.session.get('django_language', 'ko')
+
+   f = open(_get_pdf(_render_html(request)), 'rb')
+
+   response = HttpResponse(FileWrapper(f), content_type='appication/pdf')
+   activate('en')
+   response['Content-Disposition'] = 'attachment; filename=timetable%d_%d_%d.pdf' % (table_id + 1, view_year, view_semester)
+   activate(old_lang)
+
+   return response
+
+@login_required
+def save_as_image(request):
+   ''' Serve png file based on request and temlplate '''
+    table_id = int(request.GET.get('id', 0))
+    view_year = int(request.GET.get('view_year', settings.NEXT_YEAR))
+    view_semester = int(request.GET.get('view_semester', settings.NEXT_SEMESTER))
+    old_lang = request.session.get('django_language', 'ko')
+
+    f = open(_get_image(_render_html(request)), 'rb')
+
+    response = HttpResponse(FileWrapper(f), content_type='image/png')
+    activate('en')
+    response['Content-Disposition'] = 'attachment; filename=timetable%d_%d_%d.png' % (table_id + 1, view_year, view_semester)
+    activate(old_lang)
+
+    return response
