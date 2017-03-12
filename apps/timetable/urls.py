a935db6ebd138f65f323c23baf5a4d23eee224ef
ashe-sparcs <ashe@sparcs.org>
2017-03-13 02:14:07 +0900

    fetching comment done.

--- apps/timetable/urls.py
+++ apps/timetable/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     url(r'^show$', views.show_table),
     url(r'^search_temp/$', views.search_temp),
     url(r'^ajax/search_temp/$', views.search_temp_ajax),
+    url(r'^fetch_temp/$', views.fetch_temp),
+    url(r'^ajax/fetch_temp/$', views.fetch_temp_ajax),
     url(r'^search_temp$', views.search_temp),
     url(r'^ajax/search_temp$', views.search_temp_ajax),
     url(r'^api/update$', views.update_my_lectures),
