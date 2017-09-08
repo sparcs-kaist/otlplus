9518023219a7eb79f234afed12ae825a4e1ec838
hsh0908y <hsh0908y@gmail.com>
2017-09-09 00:05:46 +0900

    Apply semester settings in commands, delete legacy commands

--- db_update_script.sh
+++ db_update_script.sh
@@ -10,10 +10,10 @@ python manage.py makemigrations subject >> db_update_script.log
 python manage.py check >> db_update_script.log
 python manage.py migrate >> db_update_script.log
 python update_scholardb.py >> db_update_script.log
-python manage.py delete-needlessLecture >> db_update_script.log
 python manage.py update_ProfessorCourseList >> db_update_script.log
 python manage.py update_CourseCodenum >> db_update_script.log
 python manage.py update_CourseFiltered >> db_update_script.log
+python manage.py update_LectureTitle >> db_update_script.log
 #python update_taken_lecture.py >> db_update_script.log
 
 echo "Done!"
