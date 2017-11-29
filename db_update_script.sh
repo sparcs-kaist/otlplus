2aead47aeca7cfeb1a7b0d114649e478d7c5dfb0
hsh0908y <hsh0908y@gmail.com>
2017-11-29 14:06:13 +0900

    Remove deleted command in db_update_script.sh

--- db_update_script.sh
+++ db_update_script.sh
@@ -11,9 +11,7 @@ python manage.py check >> db_update_script.log
 python manage.py migrate >> db_update_script.log
 python update_scholardb.py >> db_update_script.log
 python manage.py update_ProfessorCourseList >> db_update_script.log
-python manage.py update_CourseCodenum >> db_update_script.log
 python manage.py update_CourseFiltered >> db_update_script.log
-python manage.py update_LectureTitle >> db_update_script.log
 #python update_taken_lecture.py >> db_update_script.log
 
 echo "Done!"
