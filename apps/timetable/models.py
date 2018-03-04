df4394bf7189233b6e708652e3148ff5dc99861e
hsh0908y <hsh0908y@gmail.com>
2018-03-04 20:11:11 +0900

    Removed unnecessary field of model Timetable

--- apps/timetable/models.py
+++ apps/timetable/models.py
@@ -6,10 +6,9 @@ from apps.session.models import *
 
 class TimeTable(models.Model):
     lecture = models.ManyToManyField(Lecture)
-    user = models.ForeignKey(UserProfile, related_name="timetable_set")  # 'UserProfile' 이부분이 좀 불안함 by ashe.
+    user = models.ForeignKey(UserProfile, related_name="timetable_set")
     year = models.IntegerField(null=True)  # 몇넌도의 타임테이블인지
     semester = models.SmallIntegerField(null=True)  # 어떤학기의 타임테이블인지
-    table_id = models.SmallIntegerField(null=True)  # 몇번째 타임테이블인지 0,1,2,3
 
 
 class OldTimeTable(models.Model):
