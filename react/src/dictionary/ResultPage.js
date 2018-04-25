1aac2d4a5ad73b2e6c699f52b0f80359ae1b9012
hsh0908y <hsh0908y@gmail.com>
2018-04-25 15:36:21 +0900

    Added dictionary router and pages

---
+++ react/src/dictionary/ResultPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from "../common/Header";
+
+class ResultPage extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <section id="content" className="container-fluid">
+                    ResultPage
+                </section>
+            </div>
+        )
+    }
+}
+
+export default ResultPage;
