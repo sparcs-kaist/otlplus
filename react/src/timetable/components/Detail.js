6eab1c413781fcdecb5485388a2f7aa6278daae1
hsh0908y <hsh0908y@gmail.com>
2018-04-30 18:46:11 +0900

    Make scroller working

--- react/src/timetable/components/Detail.js
+++ react/src/timetable/components/Detail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
 import { Link } from 'react-router-dom';
+import Scroller from '../../common/Scroller';
 
 class Detail extends Component {
     render() {
@@ -39,86 +40,84 @@ class Detail extends Component {
                                 <i className="dict-arrow"/>
                             </div>
                         </div>
-                        <div className="nano">
-                            <div className="list-scroll nano-content">
-                                <div className="basic-info">
-                                    <span className="basic-info-name fixed-ko">구분</span>
-                                    <span id="course-type">{this.props.lecture.type}</span>
+                        <Scroller>
+                            <div className="basic-info">
+                                <span className="basic-info-name fixed-ko">구분</span>
+                                <span id="course-type">{this.props.lecture.type}</span>
+                            </div>
+                            <div className="basic-info">
+                                <span className="basic-info-name fixed-ko">학과</span>
+                                <span id="department">{this.props.lecture.department_name}</span>
+                            </div>
+                            <div className="basic-info">
+                                <span className="basic-info-name fixed-ko">교수</span>
+                                <span id="instructor">{this.props.lecture.professor}</span>
+                            </div>
+                            <div className="basic-info">
+                                <span className="basic-info-name fixed-ko">장소</span>
+                                <span id="classroom">{this.props.lecture.classroom}</span>
+                            </div>
+                            <div className="basic-info">
+                                <span className="basic-info-name fixed-ko">정원</span>
+                                <span id="class-size">{this.props.lecture.limit}</span>
+                            </div>
+                            <div className="basic-info">
+                                <span className="basic-info-name fixed-ko">시험</span>
+                                <span id="exam-time">{this.props.lecture.exam}</span>
+                            </div>
+                            <div className="lecture-scores">
+                                <div className="lecture-score">
+                                    {
+                                        this.props.lecture.is_english
+                                        ? <div id="language" className="score-text">Eng</div>
+                                        : <div id="language" className="score-text score-korean">한</div>
+                                    }
+                                    <div className="score-label">언어</div>
                                 </div>
-                                <div className="basic-info">
-                                    <span className="basic-info-name fixed-ko">학과</span>
-                                    <span id="department">{this.props.lecture.department_name}</span>
+                                <div className="lecture-score">
+                                    {
+                                        this.props.lecture.credit > 0
+                                        ? <div id="credit" className="score-text">{this.props.lecture.credit}</div>
+                                        : <div id="credit" className="score-text">{this.props.lecture.credit_au}</div>
+                                    }
+                                    {
+                                        this.props.lecture.credit > 0
+                                        ? <div className="score-label">학점</div>
+                                        : <div className="score-label">AU</div>
+                                    }
                                 </div>
-                                <div className="basic-info">
-                                    <span className="basic-info-name fixed-ko">교수</span>
-                                    <span id="instructor">{this.props.lecture.professor}</span>
-                                </div>
-                                <div className="basic-info">
-                                    <span className="basic-info-name fixed-ko">장소</span>
-                                    <span id="classroom">{this.props.lecture.classroom}</span>
-                                </div>
-                                <div className="basic-info">
-                                    <span className="basic-info-name fixed-ko">정원</span>
-                                    <span id="class-size">{this.props.lecture.limit}</span>
-                                </div>
-                                <div className="basic-info">
-                                    <span className="basic-info-name fixed-ko">시험</span>
-                                    <span id="exam-time">{this.props.lecture.exam}</span>
-                                </div>
-                                <div className="lecture-scores">
-                                    <div className="lecture-score">
+                                <div className="lecture-score">
+                                    <div id="rate" className="score-text">
                                         {
-                                            this.props.lecture.is_english
-                                            ? <div id="language" className="score-text">Eng</div>
-                                            : <div id="language" className="score-text score-korean">한</div>
-                                        }
-                                        <div className="score-label">언어</div>
-                                    </div>
-                                    <div className="lecture-score">
-                                        {
-                                            this.props.lecture.credit > 0
-                                            ? <div id="credit" className="score-text">{this.props.lecture.credit}</div>
-                                            : <div id="credit" className="score-text">{this.props.lecture.credit_au}</div>
-                                        }
-                                        {
-                                            this.props.lecture.credit > 0
-                                            ? <div className="score-label">학점</div>
-                                            : <div className="score-label">AU</div>
+                                            this.props.lecture.limit === 0
+                                            ? "0.0:1"
+                                            : (this.props.lecture.num_people/this.props.lecture.limit).toFixed(1).toString()+":1"
                                         }
                                     </div>
-                                    <div className="lecture-score">
-                                        <div id="rate" className="score-text">
-                                            {
-                                                this.props.lecture.limit === 0
-                                                ? "0.0:1"
-                                                : (this.props.lecture.num_people/this.props.lecture.limit).toFixed(1).toString()+":1"
-                                            }
-                                        </div>
-                                        <div className="score-label">경쟁률</div>
-                                    </div>
-                                </div>
-                                <div className="lecture-scores">
-                                    <div className="lecture-score" style={{clear:"both"}}>
-                                        <div id="grade" className="score-text">{this.props.lecture.grade_letter}</div>
-                                        <div className="score-label">성적</div>
-                                    </div>
-                                    <div className="lecture-score">
-                                        <div id="load" className="score-text">{this.props.lecture.load_letter}</div>
-                                        <div className="score-label">널널</div>
-                                    </div>
-                                    <div className="lecture-score">
-                                        <div id="speech" className="score-text">{this.props.lecture.speech_letter}</div>
-                                        <div className="score-label">강의}</div>
-                                    </div>
-                                </div>
-                                <div className="basic-info dictionary-preview open-dict-button">
-                                    <span style={{fontWeight:"700"}}>과목 후기</span>
-                                    <i className="dict-arrow"/>
-                                </div>
-                                <div id="reviews">
+                                    <div className="score-label">경쟁률</div>
                                 </div>
                             </div>
-                        </div>
+                            <div className="lecture-scores">
+                                <div className="lecture-score" style={{clear:"both"}}>
+                                    <div id="grade" className="score-text">{this.props.lecture.grade_letter}</div>
+                                    <div className="score-label">성적</div>
+                                </div>
+                                <div className="lecture-score">
+                                    <div id="load" className="score-text">{this.props.lecture.load_letter}</div>
+                                    <div className="score-label">널널</div>
+                                </div>
+                                <div className="lecture-score">
+                                    <div id="speech" className="score-text">{this.props.lecture.speech_letter}</div>
+                                    <div className="score-label">강의}</div>
+                                </div>
+                            </div>
+                            <div className="basic-info dictionary-preview open-dict-button">
+                                <span style={{fontWeight:"700"}}>과목 후기</span>
+                                <i className="dict-arrow"/>
+                            </div>
+                            <div id="reviews">
+                            </div>
+                        </Scroller>
                     </div>
                 </div>
             );
--- react/src/timetable/components/Exam.js
+++ react/src/timetable/components/Exam.js
@@ -1,44 +1,43 @@
 import React, { Component } from 'react';
+import Scroller from "../../common/Scroller";
 
 class Exam extends Component {
     render() {
         return (
             <div style={{flex:'auto'}}>
                 <div id="examtitle"><span>시험시간표</span></div>
-                <div id="examtable" className="nano">
-                    <div className="list-scroll nano-content">
-                        <div className="exam-day" data-date="mon">
-                            <div className="exam-day-title fixed-ko">
-                                <span>월</span></div>
-                            <ul className="exam-day-body">
-                            </ul>
-                        </div>
-                        <div className="exam-day" data-date="tue">
-                            <div className="exam-day-title fixed-ko">
-                                <span>화</span></div>
-                            <ul className="exam-day-body">
-                            </ul>
-                        </div>
-                        <div className="exam-day" data-date="wed">
-                            <div className="exam-day-title fixed-ko">
-                                <span>수</span></div>
-                            <ul className="exam-day-body">
-                            </ul>
-                        </div>
-                        <div className="exam-day" data-date="thu">
-                            <div className="exam-day-title fixed-ko">
-                                <span>목</span></div>
-                            <ul className="exam-day-body">
-                            </ul>
-                        </div>
-                        <div className="exam-day" data-date="fri">
-                            <div className="exam-day-title fixed-ko">
-                                <span>금</span></div>
-                            <ul className="exam-day-body">
-                            </ul>
-                        </div>
+                <Scroller id="examtable">
+                    <div className="exam-day" data-date="mon">
+                        <div className="exam-day-title fixed-ko">
+                            <span>월</span></div>
+                        <ul className="exam-day-body">
+                        </ul>
                     </div>
-                </div>
+                    <div className="exam-day" data-date="tue">
+                        <div className="exam-day-title fixed-ko">
+                            <span>화</span></div>
+                        <ul className="exam-day-body">
+                        </ul>
+                    </div>
+                    <div className="exam-day" data-date="wed">
+                        <div className="exam-day-title fixed-ko">
+                            <span>수</span></div>
+                        <ul className="exam-day-body">
+                        </ul>
+                    </div>
+                    <div className="exam-day" data-date="thu">
+                        <div className="exam-day-title fixed-ko">
+                            <span>목</span></div>
+                        <ul className="exam-day-body">
+                        </ul>
+                    </div>
+                    <div className="exam-day" data-date="fri">
+                        <div className="exam-day-title fixed-ko">
+                            <span>금</span></div>
+                        <ul className="exam-day-body">
+                        </ul>
+                    </div>
+                </Scroller>
             </div>
         );
     }
