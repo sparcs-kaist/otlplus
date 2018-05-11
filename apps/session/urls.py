cd96bc47f779d0661cddc22502fe3b295e2ee684
zealotzealot <hsh0908y@gmail.com>
2018-05-12 01:06:00 +0900

    Implement Django view to fetch user information

--- apps/session/urls.py
+++ apps/session/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^login/callback/$', views.login_callback),
     url(r'^logout/$', views.user_logout),
     url(r'^unregister/$', views.unregister),
+    url(r'^info', views.info),
     #url(r'^unregister/callback/$', views.unregister_callback),
     url(r'^language/$', TemplateView.as_view(template_name='index.html')),
     url(r'^settings/$', TemplateView.as_view(template_name='index.html')),
