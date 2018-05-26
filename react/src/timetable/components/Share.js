fe0d9e69a942c1537c83c1f3f7cfb7de4abc4daa
Zeniuus <jsh56son@kaist.ac.kr>
2018-05-26 14:45:02 +0900

    Create buttons only in mobile view
    1. Button for timetable lists modal
    2. Button for lecture list & search

--- react/src/timetable/components/Share.js
+++ react/src/timetable/components/Share.js
@@ -4,10 +4,15 @@ class Share extends Component {
     render() {
         return (
             <div id="share-buttons" className="authenticated">
-                <a className="share-button" id="image" download>
-                </a>
-                <a className="share-button" id="calendar" target="_blank">
-                </a>
+                <div className="left-btn-group">
+                  <a className="share-button" id="image" download />
+                  <a className="share-button" id="calendar" target="_blank" />
+                </div>
+                <div className="right-btn-group">
+                  <a className="share-button" id="show-timetable-list" />
+                  <a className="share-button" id="show-lecture-list" />
+                </div>
+                <div className="height-placeholder" />
             </div>
         );
     }
