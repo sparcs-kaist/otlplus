a10f90bd7d40a43db47e9c05ad602782d6e71aa8
jooyoung <leenoi3@kaist.ac.kr>
2018-05-27 02:50:10 +0900

    view for latest changed, latest tab is done

--- apps/review/views.py
+++ apps/review/views.py
@@ -299,10 +299,8 @@ def _professor_to_dict(professor,id=-1):
 
 
 def resultProfessor(request):
-    body = json.loads(request.body.decode('utf-8'))
-    if 'q' in body :
-        keyword = body['q']
-    else :
+    keyword = request.GET.get('q')
+    if not keyword :
         keyword = ""
     if len(keyword)>0:
         expectations = Professor.objects.filter(Q(professor_name__icontains=keyword)|Q(professor_name_en__icontains=keyword))
@@ -521,9 +519,8 @@ def ReviewView(request, comment_id):
 
 
 def latest(request, page=-1):
-    body = json.loads(request.body.decode('utf-8'))
-
-    if body['filter'] == [u"F"]:
+    filter = request.GET.get('filter')
+    if filter == 'F':
         if request.user.is_authenticated():
             user_profile = UserProfile.objects.get(user=request.user)
             favorite_departments_code = []
@@ -533,7 +530,7 @@ def latest(request, page=-1):
         else:
             department_filters = []
     else:
-        department_filters = _departmentFilters(body['filter'])
+        department_filters = _departmentFilters([filter])
     comments = Comment.objects.filter(course__department__code__in=department_filters).order_by('-written_datetime')
 
     paginator = Paginator(comments,10)
