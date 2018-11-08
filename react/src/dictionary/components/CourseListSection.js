d993ef6791db7c4542df8659821913676f3aeba7
zealotzealot <hsh0908y@gmail.com>
2018-11-08 23:32:44 +0900

    Basic structure of CourseListSection

--- react/src/dictionary/components/CourseListSection.js
+++ react/src/dictionary/components/CourseListSection.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
+import Scroller from "../../common/Scroller";
+import CourseBlock from "./CourseBlock";
+
 
 class CourseListSection extends Component {
     render() {
         return (
-            <div>
-                asdf
+            <div className="section-content section-content--flex">
+                <div>
+                    전산학부
+                </div>
+                <Scroller>
+                    <CourseBlock/>
+                    <CourseBlock/>
+                    <CourseBlock/>
+                    <CourseBlock/>
+                    <CourseBlock/>
+                    <CourseBlock/>
+                </Scroller>
             </div>
         );
     }
