fa02d8fed4974fd7ed2b8421238c9395fc1e99fb
zealotzealot <hsh0908y@gmail.com>
2018-11-13 23:35:19 +0900

    Implement RelatedCoursesSection

--- react/src/common/MainPage.js
+++ react/src/common/MainPage.js
@@ -3,12 +3,18 @@ import React, { Component } from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 
+import RelatedCourseSection from "./componenets/RelatedCourseSection";
+
+
 class MainPage extends Component {
     render() {
         return (
             <div>
                 <Header user={this.props.user}/>
-                <section>
+                <section className="content">
+                    <div className="section">
+                        <RelatedCourseSection/>
+                    </div>
                 </section>
                 <Footer />
             </div>
