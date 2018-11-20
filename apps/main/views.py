9de7338e5bc1a9f583fc9a2d3cc6028731a42978
jihoon-ko <jihoonko@kaist.ac.kr>
2018-11-20 14:26:51 +0000

    fix bug for recommendation

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_POST
+from django.core.exceptions import ObjectDoesNotExist
 
 from apps.session.models import UserProfile
 from apps.subject.models import Course
 from apps.main.models import RandomCourseReco
+
+from apps.timetable.views import _lecture_to_dict
+
 import datetime
 import random
+import json
 
 # Create your views here.
 
@@ -19,7 +24,7 @@ def template(request):
 @require_POST
 def course_write_reco_load(request):
     if not request.user.is_authenticated():
-        ctx = [{'lecture':[]}]
+        ctx = {'lecture': None}
         return JsonResponse(ctx, safe=False, json_dumps_params=
                             {'ensure_ascii': False})
 
@@ -27,7 +32,8 @@ def course_write_reco_load(request):
 
     body = json.loads(request.body.decode('utf-8'))
     try:
-        target_date = int(body['target_date'])
+        yyyy, mm, dd = map(int, body['target_date'].split('-'))
+        target_date = datetime.date(yyyy, mm, dd)
     except KeyError:
         return HttpResponseBadRequest('Missing fields in request data')
 
@@ -36,16 +42,16 @@ def course_write_reco_load(request):
     rcr = None
     try:
         rcr = RandomCourseReco.objects.get(userprofile=userprofile, reco_date=target_date)
-    except DoesNotExist:
+    except ObjectDoesNotExist:
         user_taken_lecture_list = userprofile.take_lecture_list.all()
         if len(user_taken_lecture_list) == 0:
-            ctx = [{'lecture': []}]
+            ctx = {'lecture': None}
             return JsonResponse(ctx, safe=False, json_dumps_params=
                                  {'ensure_ascii': False})
         l = random.choice(user_taken_lecture_list)
         rcr = RandomCourseReco(userprofile=userprofile, reco_date=target_date, lecture=l)
         rcr.save()
 
-    ctx = [{'lecture':_lecture_result_format([rcr.lecture])}]
+    ctx = _lecture_to_dict(rcr.lecture)
     return JsonResponse(ctx, safe=False, json_dumps_params=
                         {'ensure_ascii': False})
