6c65753a6acef60158d96f7a2415e95c45b7d405
tink-expo <whhan8485@kaist.ac.kr>
2018-11-22 22:42:19 +0900

    ??

--- react/src/App.js
+++ react/src/App.js
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import axios from 'axios';
-import 'bootstrap';
-
-import './static/css/bootstrap.min.css';
-import './static/css/global.css';
-
-import TimetablePage from "./timetable/TimetablePage";
-import DictionaryRouter from "./dictionary/DictionaryRouter";
-import MainPage from "./common/MainPage";
-import CreditPage from "./common/CreditPage";
-import LicensePage from "./common/LicensePage";
-
-import timetableReducer from "./timetable/reducers/index";
-import commonReducer from "./common/reducers/index";
-import { setUser } from "./common/actions";
-
-const store = createStore(combineReducers({
-    common: commonReducer,
-    timetable: timetableReducer,
-}));
-
-class App extends Component {
-    componentDidMount() {
-        axios.get("/session/info")
-        .then((response) => {
-            store.dispatch(setUser(response.data));
-        })
-        .catch((response) => {
-            console.log(response);
-        });
-    }
-
-  render() {
-    return (
-        <Provider store={store}>
-            <Switch>
-                <Route path="/review" render={(props)=><DictionaryRouter/>}/>
-                <Route exact path="/timetable" render={(props)=><TimetablePage/>}/>
-                <Route exact path="/main" render={(props)=><MainPage/>}/>
-                <Route exact path="/credits" render={(props)=><CreditPage/>}/>
-                <Route exact path="/licenses" render={(props)=><LicensePage/>}/>
-                <Redirect from="/" to="/main/"/>
-            </Switch>
-        </Provider>
-    );
-  }
-}
-
-export default App;
-=======
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { createStore, combineReducers } from 'redux';
@@ -108,4 +53,3 @@ class App extends Component {
 }
 
 export default App;
->>>>>>> develop
