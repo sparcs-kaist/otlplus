dc2e76c5dc59a504f36a823e961a34a1b7ef4243
zealotzealot <hsh0908y@gmail.com>
2019-06-08 04:41:25 +0900

    Apply more eslint styles (4)

--- react/src/timetable/components/Review.js
+++ react/src/timetable/components/Review.js
@@ -5,16 +5,16 @@ class Review extends Component {
   render() {
     const { review } = this.props;
     return (
-      <a href={`/review/result/comment/${review.id}`} target="_blank" style={{"display":"block"}}>
+      <a href={`/review/result/comment/${review.id}`} target="_blank" rel="noopener noreferrer" style={{ display: 'block' }}>
         <div className="review-elem">
           <div className="review-body">
             {review.body}
           </div>
           <div className="review-score-wrap">
-            <span className="review-score">추천 <strong>{review.recommend}</strong></span><span
-            className="review-score">성적 <strong>{review.score}</strong></span><span
-            className="review-score">널널 <strong>{review.load}</strong></span><span
-            className="review-score">강의 <strong>{review.speech}</strong></span>
+            <span className="review-score">추천 <strong>{review.recommend}</strong></span>
+            <span className="review-score">성적 <strong>{review.score}</strong></span>
+            <span className="review-score">널널 <strong>{review.load}</strong></span>
+            <span className="review-score">강의 <strong>{review.speech}</strong></span>
           </div>
         </div>
       </a>
