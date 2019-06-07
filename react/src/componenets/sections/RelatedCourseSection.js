ae9b725db54fb427fd1a9aeb39d35b0df9a0230e
zealotzealot <hsh0908y@gmail.com>
2019-06-07 22:05:01 +0900

    Apply ESLint styles (5)

--- react/src/componenets/sections/RelatedCourseSection.js
+++ react/src/componenets/sections/RelatedCourseSection.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 
-import CourseBlock from "../blocks/CourseBlock";
+import CourseBlock from '../blocks/CourseBlock';
 
 
 class RelatedCourseSection extends Component {
-    render() {
-        return (
-            <div className="section-content section-content--widget">
-                <div className="title">
-                    연관 과목 - 데이타구조
-                </div>
-                <CourseBlock/>
-                <CourseBlock/>
-                <CourseBlock/>
-                <div className="buttons">
-                    <button className="text-button">
-                        자세히 보기
-                    </button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="section-content section-content--widget">
+        <div className="title">
+          연관 과목 - 데이타구조
+        </div>
+        <CourseBlock />
+        <CourseBlock />
+        <CourseBlock />
+        <div className="buttons">
+          <button className="text-button">
+            자세히 보기
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 
