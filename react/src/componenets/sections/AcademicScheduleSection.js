7edaff955b11bfe7018be31f63713ead313299d3
zealotzealot <hsh0908y@gmail.com>
2019-06-07 22:05:01 +0900

    Apply ESLint styles (5)

--- react/src/componenets/sections/AcademicScheduleSection.js
+++ react/src/componenets/sections/AcademicScheduleSection.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 
 
 class AcademicScheduleSection extends Component {
-    render() {
-        return (
-            <div className="section-content section-content--widget">
-                <div className="academic-schedule">
-                    <div>
-                        D-4일 13시간 22분 07초
-                    </div>
-                    <div>
-                        <strong>봄학기 수강취소 마감</strong><span>2018.1.3</span>
-                    </div>
-                </div>
-                <div className="buttons">
-                    <button className="text-button">
-                        학사시스템 바로가기
-                    </button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="section-content section-content--widget">
+        <div className="academic-schedule">
+          <div>
+            D-4일 13시간 22분 07초
+          </div>
+          <div>
+            <strong>봄학기 수강취소 마감</strong><span>2018.1.3</span>
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="text-button">
+            학사시스템 바로가기
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 
