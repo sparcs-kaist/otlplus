da8b49c0799f57b07817a0f564d48910f4601d17
zealotzealot <hsh0908y@gmail.com>
2019-06-07 20:48:57 +0900

    Apply ESLint styles (2)

--- react/src/pages/DictionaryPage.js
+++ react/src/pages/DictionaryPage.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 
-import Header from "../componenets/Header";
-import CourseListSection from "../componenets/sections/CourseListSection";
-import CourseDetailSection from "../componenets/sections/CourseDetailSection";
+import Header from '../componenets/Header';
+import CourseListSection from '../componenets/sections/CourseListSection';
+import CourseDetailSection from '../componenets/sections/CourseDetailSection';
 
-import "../App.scss";
+import '../App.scss';
 
 
 class DictionaryPage extends Component {
-    render() {
-        return (
-            <div>
-                <Header user={this.props.user}/>
-                <section className="content content--no-scroll">
+  render() {
+    return (
+      <div>
+        <Header user={this.props.user} />
+        <section className="content content--no-scroll">
                     <div className="section-wrap section-wrap--course-list">
-                        <div className="tab--course-list">
-                        </div>
-                        <div className="section section--with-tabs">
-                            <CourseListSection/>
-                        </div>
+                      <div className="tab--course-list">
+                      </div>
+                      <div className="section section--with-tabs">
+                        <CourseListSection />
+                      </div>
                     </div>
                     <div className="section-wrap section-wrap--course-detail">
-                        <div className="section">
-                            <CourseDetailSection/>
-                        </div>
+                      <div className="section">
+                        <CourseDetailSection />
+                      </div>
                     </div>
-                </section>
-            </div>
-        );
-    }
+        </section>
+      </div>
+    );
+  }
 }
 
 
