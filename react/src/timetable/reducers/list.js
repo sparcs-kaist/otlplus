3267004ff7c789557abb1662983137d16de1d116
zealotzealot <hsh0908y@gmail.com>
2019-06-08 03:48:24 +0900

    Apply more eslint styles (3)

--- react/src/timetable/reducers/list.js
+++ react/src/timetable/reducers/list.js
@@ -1214,20 +1214,23 @@ export const list = (state = initialState, action) => {
       });
     case SET_LIST_LECTURES:
       const groupLectures = (lectures) => {
-        if (lectures.length === 0)
+        if (lectures.length === 0) {
           return [];
+        }
 
-        let courses = [[lectures[0]]];
-        for (let i=1, lecture; lectures[i]!==undefined; i++) {
-          lecture=lectures[i];
-          if (lecture.course === courses[courses.length-1][0].course)
-            courses[courses.length-1].push(lecture);
-          else
+        const courses = [[lectures[0]]];
+        for (let i = 1, lecture; lectures[i] !== undefined; i++) {
+          lecture = lectures[i];
+          if (lecture.course === courses[courses.length - 1][0].course) {
+            courses[courses.length - 1].push(lecture);
+          }
+          else {
             courses.push([lecture]);
+          }
         }
         return courses;
       };
-      let newState = {};
+      const newState = {};
       newState[action.code] = {
         ...state[action.code],
         courses: groupLectures(action.lectures),
@@ -1235,33 +1238,39 @@ export const list = (state = initialState, action) => {
       return Object.assign({}, state, newState);
     case ADD_LECTURE_TO_CART:
       let courses = state.cart.courses;
-      let i;
-      let course;
-      for (i=0; (course=courses[i]); i++) {
-        if (course[0].old_code < action.lecture.old_code)
+      let breakFlag = false;
+      for (let i = 0, course; (course = courses[i]); i++) {
+        if (course[0].old_code < action.lecture.old_code) {
           continue;
+        }
         else if (course[0].old_code === action.lecture.old_code) {
           let j;
           let lecture;
-          for (j=0; (lecture=course[j]); j++) {
-            if (lecture.class_no < action.lecture.class_no)
+          for (j = 0; (lecture = course[j]); j++) {
+            if (lecture.class_no < action.lecture.class_no) {
               continue;
+            }
             else {
               course.splice(j, 0, action.lecture);
+              breakFlag = true;
               break;
             }
           }
-          if (j === course.length)
+          if (j === course.length) {
             course.push(action.lecture);
+          }
+          breakFlag = true;
           break;
         }
         else {
           courses.splice(i, 0, [action.lecture]);
+          breakFlag = true;
           break;
         }
       }
-      if (i === courses.length)
+      if (!breakFlag) {
         courses.push([action.lecture]);
+      }
       return Object.assign({}, state, {
         cart: {
           courses: courses,
@@ -1269,12 +1278,13 @@ export const list = (state = initialState, action) => {
       });
     case DELETE_LECTURE_FROM_CART:
       courses = state.cart.courses;
-      for (let i=0, course; (course=courses[i]); i++) {
-        for (let j=0, lecture; (lecture=course[j]); j++)
+      for (let i = 0, course; (course = courses[i]); i++) {
+        for (let j = 0, lecture; (lecture = course[j]); j++) {
           if (lecture.id === action.lecture.id) {
             course.splice(j, 1);
             j--;
           }
+        }
         if (course.length === 0) {
           courses.splice(i, 1);
           i--;
