1d1e7bf9f46fdc0f2321e4d5ba88ac94728479fe
zealotzealot <hsh0908y@gmail.com>
2019-06-09 23:56:20 +0900

    Moved methods and redux connections of TimetableBlock to its parent

--- react/src/componenets/blocks/TimetableBlock.js
+++ react/src/componenets/blocks/TimetableBlock.js
@@ -1,50 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import axios from '../../presetAxios';
-import { setLectureActive, clearLectureActive, removeLectureFromTimetable, lectureinfo } from '../../actions/timetable/index';
 
 class TimetableBlock extends Component {
-  blockHover = lecture => () => {
-    if (!this.props.lectureActiveClicked && !this.props.isDragging) {
-      this.props.setLectureActiveDispatch(lecture, 'TABLE', false);
-    }
-  }
-
-  blockOut = () => {
-    if (!this.props.lectureActiveClicked) {
-      this.props.clearLectureActiveDispatch();
-    }
-  }
-
-  blockClick = lecture => () => {
-    if (this.props.lectureActiveClicked
-      && this.props.lectureActiveFrom === 'TABLE'
-      && this.props.lectureActiveLecture.id === lecture.id) {
-      this.props.setLectureActiveDispatch(lecture, 'TABLE', false);
-      this.props.lectureinfoDispatch();
-    }
-    else {
-      this.props.setLectureActiveDispatch(lecture, 'TABLE', true);
-      this.props.lectureinfoDispatch();
-    }
-  }
-
-  deleteLecture = lecture => (event) => {
-    event.stopPropagation();
-
-    axios.post('/api/timetable/table_update', {
-      table_id: this.props.currentTimetable.id,
-      lecture_id: lecture.id,
-      delete: true,
-    })
-      .then((response) => {
-        this.props.removeLectureFromTimetableDispatch(lecture);
-      })
-      .catch((response) => {
-        console.log(response);
-      });
-  }
-
   render() {
     const indexOfTime = time => (time / 30 - 16);
 
@@ -70,11 +26,11 @@ class TimetableBlock extends Component {
           width: this.props.cellWidth + 2,
           height: this.props.cellHeight * (indexOfTime(this.props.classtime.end) - indexOfTime(this.props.classtime.begin)) - 3,
         }}
-        onMouseOver={() => this.blockHover(this.props.lecture)()}
-        onMouseOut={() => this.blockOut()}
-        onClick={() => this.blockClick(this.props.lecture)()}
+        onMouseOver={() => this.props.blockHover(this.props.lecture)()}
+        onMouseOut={() => this.props.blockOut()}
+        onClick={() => this.props.blockClick(this.props.lecture)()}
       >
-        <div className="lecture-delete" onClick={event => this.deleteLecture(this.props.lecture)(event)}><i /></div>
+        <div className="lecture-delete" onClick={event => this.props.deleteLecture(this.props.lecture)(event)}><i /></div>
         <div
           // onMouseDown={() => this.props.onMouseDown()}
           className="lecture-block-content"
@@ -93,33 +49,5 @@ class TimetableBlock extends Component {
     );
   }
 }
-
-const mapStateToProps = state => ({
-  cellWidth: state.timetable.timetable.cellWidth,
-  cellHeight: state.timetable.timetable.cellHeight,
-  lectureActiveFrom: state.timetable.lectureActive.from,
-  lectureActiveClicked: state.timetable.lectureActive.clicked,
-  lectureActiveLecture: state.timetable.lectureActive.lecture,
-  showLectureInfoFlag: state.timetable.mobile.showLectureInfoFlag,
-  isDragging: state.timetable.timetable.isDragging,
-  currentTimetable: state.timetable.timetable.currentTimetable,
-});
-
-const mapDispatchToProps = dispatch => ({
-  setLectureActiveDispatch: (lecture, from, clicked) => {
-    dispatch(setLectureActive(lecture, from, clicked));
-  },
-  clearLectureActiveDispatch: () => {
-    dispatch(clearLectureActive());
-  },
-  removeLectureFromTimetableDispatch: (lecture) => {
-    dispatch(removeLectureFromTimetable(lecture));
-  },
-  lectureinfoDispatch: () => {
-    dispatch(lectureinfo());
-  },
-});
-
-TimetableBlock = connect(mapStateToProps, mapDispatchToProps)(TimetableBlock);
 
 export default TimetableBlock;
