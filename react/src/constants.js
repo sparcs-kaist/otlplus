496015bded68640f3711b09602edee600a41acc7
zealotzealot <hsh0908y@gmail.com>
2019-06-12 20:09:03 +0900

    Implement local BASE_URL setting

--- react/src/constants.js
+++ react/src/constants.js
@@ -1,1 +1,13 @@
-export const BASE_URL = ''; // Use Relative URL
+let LocalConstants = {};
+try {
+  LocalConstants = require('./localConstants');
+}
+catch (e) {
+  // Nothing
+}
+
+export const BASE_URL = (
+  (process.env.NODE_ENV === 'development' && LocalConstants.BASE_URL)
+    ? LocalConstants.BASE_URL
+    : '' // Use Relative URL
+);
