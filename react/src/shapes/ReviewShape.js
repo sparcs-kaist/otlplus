14a061dbbcd832593775e009f9d3e39d7416d62c
zealotzealot <hsh0908y@gmail.com>
2019-06-20 02:53:00 +0900

    Update Django Comment.toJson(), use it on view comment_load, and update related React files

--- react/src/shapes/ReviewShape.js
+++ react/src/shapes/ReviewShape.js
@@ -1,13 +1,22 @@
 import PropTypes from 'prop-types';
 
+import CourseShape from './CourseShape';
+import LectureShape from './LectureShape';
+
 
 const reviewShape = PropTypes.shape({
-  grade: PropTypes.string.isRequired,
-  load: PropTypes.string.isRequired,
-  speech: PropTypes.string.isRequired,
-  like: PropTypes.number.isRequired,
-  comment: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
+  course: CourseShape.isRequired,
+  lecture: LectureShape.isRequired,
+  comment: PropTypes.string.isRequired,
+  like: PropTypes.number.isRequired,
+  is_deleted: PropTypes.bool.isRequired,
+  grade: PropTypes.number.isRequired,
+  load: PropTypes.number.isRequired,
+  speech: PropTypes.number.isRequired,
+  grade_letter: PropTypes.string.isRequired,
+  load_letter: PropTypes.string.isRequired,
+  speech_letter: PropTypes.string.isRequired,
 });
 
 export default reviewShape;
