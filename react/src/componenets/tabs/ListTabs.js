0a65cf2a8e758370736c109e0c1af7f1f7859d9a
zealotzealot <hsh0908y@gmail.com>
2019-06-20 23:10:02 +0900

    Use object destructuring in state and props (3)

--- react/src/componenets/tabs/ListTabs.js
+++ react/src/componenets/tabs/ListTabs.js
@@ -9,29 +9,33 @@ import lectureShape from '../../shapes/LectureShape';
 
 class ListTabs extends Component {
   changeTab = (list) => {
-    this.props.setCurrentListDispatch(list);
+    const { search, lectureActiveFrom, setCurrentListDispatch, openSearchDispatch, closeSearchDispatch, clearLectureActiveDispatch } = this.props;
 
-    if (list === 'SEARCH' && (this.props.search.courses === null || this.props.search.courses.length === 0)) {
-      this.props.openSearchDispatch();
+    setCurrentListDispatch(list);
+
+    if (list === 'SEARCH' && (search.courses === null || search.courses.length === 0)) {
+      openSearchDispatch();
     }
     else {
-      this.props.closeSearchDispatch();
+      closeSearchDispatch();
     }
 
-    if (this.props.lectureActiveFrom === LIST) {
-      this.props.clearLectureActiveDispatch();
+    if (lectureActiveFrom === LIST) {
+      clearLectureActiveDispatch();
     }
   }
 
   render() {
+    const { currentList, major } = this.props;
+
     return (
       <div id="list-tab-wrap">
-        <button className={`list-tab search${this.props.currentList === 'SEARCH' ? ' active' : ''}`} onClick={() => this.changeTab('SEARCH')}><i className="list-tab-icon" /></button>
-        {this.props.major.codes.map(code => (
-          <button className={`list-tab major${this.props.currentList === code ? ' active' : ''}`} key={code} onClick={() => this.changeTab(code)}><i className="list-tab-icon" /></button>
+        <button className={`list-tab search${currentList === 'SEARCH' ? ' active' : ''}`} onClick={() => this.changeTab('SEARCH')}><i className="list-tab-icon" /></button>
+        {major.codes.map(code => (
+          <button className={`list-tab major${currentList === code ? ' active' : ''}`} key={code} onClick={() => this.changeTab(code)}><i className="list-tab-icon" /></button>
         ))}
-        <button className={`list-tab humanity${this.props.currentList === 'HUMANITY' ? ' active' : ''}`} onClick={() => this.changeTab('HUMANITY')}><i className="list-tab-icon" /></button>
-        <button className={`list-tab cart${this.props.currentList === 'CART' ? ' active' : ''}`} onClick={() => this.changeTab('CART')}><i className="list-tab-icon" /></button>
+        <button className={`list-tab humanity${currentList === 'HUMANITY' ? ' active' : ''}`} onClick={() => this.changeTab('HUMANITY')}><i className="list-tab-icon" /></button>
+        <button className={`list-tab cart${currentList === 'CART' ? ' active' : ''}`} onClick={() => this.changeTab('CART')}><i className="list-tab-icon" /></button>
       </div>
     );
   }
