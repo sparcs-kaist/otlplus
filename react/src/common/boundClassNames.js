d57a9eb75cfe1eb196a72eaa8dd2928ad309e406
zealotzealot <hsh0908y@gmail.com>
2019-06-21 18:37:25 +0900

    Convert credits.css to css module

--- react/src/common/boundClassNames.js
+++ react/src/common/boundClassNames.js
@@ -2,6 +2,8 @@ import classNames from 'classnames/bind';
 
 import appStyles from '../App.module.scss';
 import timetablStyles from '../styles/timetable.module.css';
+import creditStyles from '../styles/credit.module.css';
 
 export const appBoundClassNames = classNames.bind(appStyles);
 export const timetableBoundClassNames = classNames.bind(timetablStyles);
+export const creditBoundClassNames = classNames.bind(creditStyles);
