731c5232daeab041a11b15ba8fa13e4e6a53cf03
zealotzealot <hsh0908y@gmail.com>
2019-06-22 19:28:05 +0000

    Update db_update_script.sh to find its own directory

--- db_update_script.sh
+++ db_update_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Start!"
 echo $(date +'%Y-%m-%d / %T') > db_update_script.log
-
-python manage.py makemigrations >> db_update_script.log
-python manage.py makemigrations review >> db_update_script.log
-python manage.py makemigrations session >> db_update_script.log
-python manage.py makemigrations timetable >> db_update_script.log
-python manage.py makemigrations subject >> db_update_script.log
-python manage.py check >> db_update_script.log
-python manage.py migrate >> db_update_script.log
-python update_scholardb.py >> db_update_script.log
-python manage.py update_ProfessorCourseList >> db_update_script.log
-python manage.py update_CourseFiltered >> db_update_script.log
-#python update_taken_lecture.py >> db_update_script.log
+BASEDIR=$(dirname $(readlink -f "$0"))
+python $BASEDIR/manage.py makemigrations >> db_update_script.log
+python $BASEDIR/manage.py makemigrations review >> db_update_script.log
+python $BASEDIR/manage.py makemigrations session >> db_update_script.log
+python $BASEDIR/manage.py makemigrations timetable >> db_update_script.log
+python $BASEDIR/manage.py makemigrations subject >> db_update_script.log
+python $BASEDIR/manage.py check >> db_update_script.log
+python $BASEDIR/manage.py migrate >> db_update_script.log
+python $BASEDIR/update_scholardb.py >> db_update_script.log
+python $BASEDIR/manage.py update_ProfessorCourseList >> db_update_script.log
+python $BASEDIR/manage.py update_CourseFiltered >> db_update_script.log
+python $BASEDIR/update_taken_lecture.py >> db_update_script.log
 
 echo "Done!"
