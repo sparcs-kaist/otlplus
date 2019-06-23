bc8f483df830cde4a40ff46e4e777a1cd7d6ca4a
zealotzealot <hsh0908y@gmail.com>
2019-06-24 01:57:10 +0900

    Add default paramsSerializer to presetAxios using qs

--- react/src/common/presetAxios.js
+++ react/src/common/presetAxios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import Qs from 'qs';
 
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.xsrfCookieName = 'csrftoken';
+
+axios.defaults.paramsSerializer = params => Qs.stringify(params, { arrayFormat: 'repeat' });
 
 export default axios;
