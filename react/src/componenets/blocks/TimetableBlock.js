05baa28fbdb59770fe2db83d2d5da6a67a1a8d7b
zealotzealot <hsh0908y@gmail.com>
2019-10-17 02:54:06 +0900

    Fix lectures in timetable having wrong color

--- react/src/componenets/blocks/TimetableBlock.js
+++ react/src/componenets/blocks/TimetableBlock.js
@@ -23,7 +23,7 @@ const TimetableBlock = ({ lecture, classtime, cellWidth, cellHeight, isClicked, 
   return (
       // eslint-disable-next-line react/jsx-indent
       <div
-        className={classNames('block--timetable', `background-color--${lecture.course % 16}`, activeType)}
+        className={classNames('block--timetable', `background-color--${lecture.course % 16 + 1}`, activeType)}
         style={{
           left: (cellWidth + 5) * classtime.day + 17,
           top: cellHeight * indexOfTime(classtime.begin) + 19,
