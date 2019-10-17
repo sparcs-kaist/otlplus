18401fa565556bd12d3b30bcd1703b134609f1f2
zealotzealot <hsh0908y@gmail.com>
2019-10-17 21:21:33 +0900

    Implement CourseListTabs

---
+++ react/src/componenets/tabs/CourseListTabs.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { appBoundClassNames as classNames } from '../../common/boundClassNames';
+
+import { /* openSearch, closeSearch, */setCurrentList/* , clearLectureActive */ } from '../../actions/dictionary/list';
+// import { NONE, LIST, TABLE, MULTIPLE } from '../../reducers/timetable/lectureActive';
+import courseShape from '../../shapes/CourseShape';
+
+
+class CourseListTabs extends Component {
+  changeTab = (list) => {
+    const { search, setCurrentListDispatch/* , openSearchDispatch, closeSearchDispatch, clearLectureActiveDispatch */ } = this.props;
+
+    setCurrentListDispatch(list);
+
+    if (list === 'SEARCH' && (search.courses === null || search.courses.length === 0)) {
+      // openSearchDispatch();
+    }
+    else {
+      // closeSearchDispatch();
+    }
+
+    // clearLectureActiveDispatch();
+  }
+
+  render() {
+    const { currentList, major } = this.props;
+
+    return (
+      <div className={classNames('tab--lecture-list')}>
+        <button className={classNames((currentList === 'SEARCH' ? 'tab__elem--active' : ''))} onClick={() => this.changeTab('SEARCH')}><i className={classNames('icon', 'icon--block-search')} /></button>
+        {major.codes.map(code => (
+          <button className={classNames((currentList === code ? 'tab__elem--active' : ''))} key={code} onClick={() => this.changeTab(code)}><i className={classNames('icon', 'icon--block-major')} /></button>
+        ))}
+        <button className={classNames((currentList === 'HUMANITY' ? 'tab__elem--active' : ''))} onClick={() => this.changeTab('HUMANITY')}><i className={classNames('icon', 'icon--block-humanity')} /></button>
+        <button className={classNames((currentList === 'TAKEN' ? 'tab__elem--active' : ''))} onClick={() => this.changeTab('TAKEN')}><i className={classNames('icon', 'icon--block-taken')} /></button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  currentList: state.dictionary.list.currentList,
+  search: state.dictionary.list.search,
+  major: state.dictionary.list.major,
+  humanity: state.dictionary.list.humanity,
+  taken: state.dictionary.list.taken,
+});
+
+const mapDispatchToProps = dispatch => ({
+  /*
+  openSearchDispatch: () => {
+    dispatch(openSearch());
+  },
+  closeSearchDispatch: () => {
+    dispatch(closeSearch());
+  },
+  */
+  setCurrentListDispatch: (list) => {
+    dispatch(setCurrentList(list));
+  },
+  /*
+  clearLectureActiveDispatch: () => {
+    dispatch(clearLectureActive());
+  },
+  */
+});
+
+CourseListTabs.propTypes = {
+  currentList: PropTypes.string.isRequired,
+  search: PropTypes.shape({
+    courses: PropTypes.arrayOf(PropTypes.arrayOf(courseShape)),
+  }).isRequired,
+  major: PropTypes.shape({
+    codes: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+  humanity: PropTypes.shape({
+    courses: PropTypes.arrayOf(PropTypes.arrayOf(courseShape)),
+  }).isRequired,
+  taken: PropTypes.shape({
+    courses: PropTypes.arrayOf(PropTypes.arrayOf(courseShape)),
+  }).isRequired,
+  /*
+  openSearchDispatch: PropTypes.func.isRequired,
+  closeSearchDispatch: PropTypes.func.isRequired,
+  */
+  setCurrentListDispatch: PropTypes.func.isRequired,
+  /*
+  clearLectureActiveDispatch: PropTypes.func.isRequired,
+  */
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CourseListTabs);
