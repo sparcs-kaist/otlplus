2a8a7af81ab6f2557e6b3748a7f43cbb5bd3be5f
zealotzealot <hsh0908y@gmail.com>
2019-10-28 12:49:38 +0900

    Add field reviews and lectures Redux courseActive

--- react/src/reducers/dictionary/courseActive.js
+++ react/src/reducers/dictionary/courseActive.js
@@ -1,22 +1,41 @@
-import { SET_COURSE_ACTIVE, CLEAR_COURSE_ACTIVE } from '../../actions/dictionary/courseActive';
+import { SET_COURSE_ACTIVE, CLEAR_COURSE_ACTIVE, SET_REVIEWS, SET_LECTURES } from '../../actions/dictionary/courseActive';
 
 const initialState = {
   clicked: false,
   course: null,
+  reviews: null,
+  lectures: null,
 };
 
 const courseActive = (state = initialState, action) => {
   switch (action.type) {
     case SET_COURSE_ACTIVE: {
+      const courseChanged = !state.course || (state.course.id !== action.course.id);
       return Object.assign({}, state, {
         clicked: action.clicked,
         course: action.course,
-      });
+      },
+      (courseChanged
+        ? { reviews: null, lectures: null }
+        : {}
+      ));
     }
     case CLEAR_COURSE_ACTIVE: {
       return Object.assign({}, state, {
         clicked: false,
         course: null,
+        reviews: null,
+        lectures: null,
+      });
+    }
+    case SET_REVIEWS: {
+      return Object.assign({}, state, {
+        reviews: action.reviews,
+      });
+    }
+    case SET_LECTURES: {
+      return Object.assign({}, state, {
+        lectures: action.lectures,
       });
     }
     default: {
