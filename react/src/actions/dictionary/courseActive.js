b60dac4db846c1d1d7c19c48a7947d0b00ec3b9e
zealotzealot <hsh0908y@gmail.com>
2019-10-28 12:49:38 +0900

    Add field reviews and lectures Redux courseActive

--- react/src/actions/dictionary/courseActive.js
+++ react/src/actions/dictionary/courseActive.js
@@ -1,5 +1,7 @@
 export const SET_COURSE_ACTIVE = 'SET_COURSE_ACTIVE';
 export const CLEAR_COURSE_ACTIVE = 'CLEAR_COURSE_ACTIVE';
+export const SET_REVIEWS = 'SET_REVIEWS';
+export const SET_LECTURES = 'SET_LECTURES';
 
 export function setCourseActive(course, clicked) {
   return {
@@ -12,5 +14,19 @@ export function setCourseActive(course, clicked) {
 export function clearCourseActive() {
   return {
     type: CLEAR_COURSE_ACTIVE,
+  };
+}
+
+export function setReviews(reviews) {
+  return {
+    type: SET_REVIEWS,
+    reviews: reviews,
+  };
+}
+
+export function setLectures(lectures) {
+  return {
+    type: SET_LECTURES,
+    lectures: lectures,
   };
 }
