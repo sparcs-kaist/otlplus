81f9c989fd294ba32fb21c2ba3ce5f99f1d04432
zealotzealot <hsh0908y@gmail.com>
2019-11-23 14:17:42 +0900

    Apply links

--- react/src/components/sections/main/RelatedCourseSection.js
+++ react/src/components/sections/main/RelatedCourseSection.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import { appBoundClassNames as classNames } from '../../../common/boundClassNames';
 import CourseBlock from '../../blocks/CourseBlock';
@@ -12,11 +13,17 @@ class RelatedCourseSection extends Component {
         <div className={classNames('title')}>
           연관 과목 - 데이타구조
         </div>
-        { courses.map(c => <CourseBlock course={c} key={c.id} />) }
+        { courses.map(c => (
+          <Link to={{ pathname: '/dictionary', state: { startCourseId: c.id } }}>
+            <CourseBlock course={c} key={c.id} />
+          </Link>
+        ))}
         <div className={classNames('buttons')}>
+        <Link to={{ pathname: '/dictionary', state: { startCourseId: 746 } }}>
           <button className={classNames('text-button')}>
             자세히 보기
           </button>
+        </Link>
         </div>
       </div>
     );
