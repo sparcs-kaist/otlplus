fe7766a6767bb1fe9cebd519e26ed7730f96becc
zealotzealot <hsh0908y@gmail.com>
2019-12-27 19:48:42 +0900

    Apply i18n: CurrentTimetableBlock and ReviewWriteBlock

--- react/src/components/blocks/ReviewWriteBlock.js
+++ react/src/components/blocks/ReviewWriteBlock.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { pure } from 'recompose';
+import { withTranslation } from 'react-i18next';
 
 import { appBoundClassNames as classNames } from '../../common/boundClassNames';
 import lectureShape from '../../shapes/NestedLectureShape';
 
 
 // eslint-disable-next-line arrow-body-style
-const ReviewWriteBlock = ({ lecture }) => {
+const ReviewWriteBlock = ({ t, lecture }) => {
   return (
     <form className={classNames('block', 'block--review-write')}>
       <div className={classNames('block--review-write__title')}>
-        <strong>{lecture.title}</strong>
-        <span>{lecture.professor_short}</span>
-        <span>{`${lecture.year} ${[undefined, '봄', '여름', '가을', '겨울'][lecture.semester]}`}</span>
+        <strong>{lecture[t('js.property.title')]}</strong>
+        <span>{lecture[t('js.property.professor_short')]}</span>
+        <span>{`${lecture.year} ${[undefined, t('ui.semester.spring'), t('ui.semester.summer'), t('ui.semester.fall'), t('ui.semester.winter')][lecture.semester]}`}</span>
       </div>
-      <textarea className={classNames('block--review-write__content')} placeholder="학점, 로드 등의 평가에 대하여 왜 그렇게 평가를 했는지 서술해주세요." />
+      <textarea className={classNames('block--review-write__content')} placeholder={t('ui.placeholder.reviewContent')} />
       <div>
         <div className={classNames('block--review-write__score')}>
-          <span className={classNames('block--review-write__score__name')}>성적</span>
+          <span className={classNames('block--review-write__score__name')}>{t('ui.score.grade')}</span>
           <label className={classNames('block--review-write__score__option')}>
             <input type="radio" name="grade" value="A" />
             <span>A</span>
@@ -40,7 +41,7 @@ const ReviewWriteBlock = ({ lecture }) => {
           </label>
         </div>
         <div className={classNames('block--review-write__score')}>
-          <span className={classNames('block--review-write__score__name')}>널널</span>
+          <span className={classNames('block--review-write__score__name')}>{t('ui.score.load')}</span>
           <label className={classNames('block--review-write__score__option')}>
             <input type="radio" name="load" value="A" />
             <span>A</span>
@@ -63,7 +64,7 @@ const ReviewWriteBlock = ({ lecture }) => {
           </label>
         </div>
         <div className={classNames('block--review-write__score')}>
-          <span className={classNames('block--review-write__score__name')}>강의</span>
+          <span className={classNames('block--review-write__score__name')}>{t('ui.score.speech')}</span>
           <label className={classNames('block--review-write__score__option')}>
             <input type="radio" name="speech" value="B" />
             <span>B</span>
@@ -88,7 +89,7 @@ const ReviewWriteBlock = ({ lecture }) => {
       </div>
       <div className={classNames('block--review-write__buttons')}>
         <button className={classNames('text-button', 'text-button--review-write-block')} type="submit">
-          업로드
+          {t('ui.button.upload')}
         </button>
       </div>
     </form>
@@ -99,4 +100,4 @@ ReviewWriteBlock.propTypes = {
   lecture: lectureShape.isRequired,
 };
 
-export default pure(ReviewWriteBlock);
+export default withTranslation()(pure(ReviewWriteBlock));
