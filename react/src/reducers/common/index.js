6450c6f5a6728d937e5eb55c6812bf068902f89b
zealotzealot <hsh0908y@gmail.com>
2019-12-30 00:04:24 +0900

    Implement and apply semesters redux

--- react/src/reducers/common/index.js
+++ react/src/reducers/common/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import semester from './semester';
 
 const CombinedReducer = combineReducers({
-    user: user,
+  user: user,
+  semester: semester,
 });
 
 export default CombinedReducer;
