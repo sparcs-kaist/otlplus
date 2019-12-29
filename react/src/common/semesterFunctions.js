2a652d82cfe7f4250c91fe48b67683eaf7cef6a6
zealotzealot <hsh0908y@gmail.com>
2019-12-30 00:48:09 +0900

    Implement using semester data in AcademicScheduleSection

---
+++ react/src/common/semesterFunctions.js
@@ -0,0 +1,30 @@
+import { SCHEDULE_FIELDS } from '../shapes/SemesterShape';
+
+export const getTimetableSemester = (semesters) => {
+  // eslint-disable-next-line fp/no-mutating-methods
+  const semestersDescending = semesters
+    .slice()
+    .sort((a, b) => (b - a));
+  const timetableSemester = semestersDescending.find(s => s.start);
+  return timetableSemester;
+};
+
+export const getCurrentSchedule = (semesters) => {
+  // eslint-disable-next-line fp/no-mutating-methods
+  const allSchedules = semesters
+    .map(s => (
+      SCHEDULE_FIELDS.map(f => ({
+        year: s.year,
+        semester: s.semester,
+        type: f,
+        time: new Date(s[f]),
+      }))
+    ))
+    .flat()
+    .sort((a, b) => (a.time - b.time));
+  const now = Date.now();
+  const currentSchedule = allSchedules
+    .find(s => (s.time > now));
+
+  return currentSchedule;
+};
