052ffdf308662969eb74488781772ff90f4ad399
zealotzealot <hsh0908y@gmail.com>
2019-12-30 01:51:45 +0900

    Move list fetching from LectureListSection to LectureListSection

--- react/src/components/tabs/LectureListTabs.js
+++ react/src/components/tabs/LectureListTabs.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
 import { appBoundClassNames as classNames } from '../../common/boundClassNames';
+import axios from '../../common/presetAxios';
 
+import { BASE_URL } from '../../common/constants';
 import { clearLectureActive } from "../../actions/timetable/lectureActive";
-import { setListMajorCodes, setCurrentList } from '../../actions/timetable/list';
+import { setListMajorCodes, setCurrentList, setListLectures, clearListsLectures, setListMajorLectures } from '../../actions/timetable/list';
 import { openSearch, closeSearch } from '../../actions/timetable/search';
 import { NONE, LIST, TABLE, MULTIPLE } from '../../reducers/timetable/lectureActive';
 import userShape from '../../shapes/UserShape';
@@ -23,11 +25,83 @@ class LectureListTabs extends Component {
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    const { user, setListMajorCodesDispatch } = this.props;
+    const { user, major, year, semester, setListMajorCodesDispatch, clearListsLecturesDispatch } = this.props;
 
     if (user && (user !== prevProps.user)) {
       setListMajorCodesDispatch(user.departments);
     }
+
+    if (!this._codesAreSame(major.codes, prevProps.major.codes)) {
+      this._fetchLists(true);
+    }
+
+    if (year !== prevProps.year || semester !== prevProps.semester) {
+      clearListsLecturesDispatch();
+      this._fetchLists(false);
+    }
+  }
+
+  _codesAreSame = (codes1, codes2) => (
+    codes1.length === codes2.length
+    && codes1.every((c, i) => (c === codes2[i]))
+  )
+
+  _fetchLists = (majorOnly) => {
+    const { year, semester, major, setListMajorLecturesDispatch, setListLecturesDispatch } = this.props;
+    const majorCodes = major.codes;
+
+    axios.get(`${BASE_URL}/api/lectures`, { params: {
+      year: year,
+      semester: semester,
+      group: majorCodes,
+    } })
+      .then((response) => {
+        const newProps = this.props;
+        if ((newProps.year !== year || newProps.semester !== semester)
+          || !this._codesAreSame(newProps.major.codes, majorCodes)
+        ) {
+          return;
+        }
+        major.codes.forEach((code) => {
+          setListMajorLecturesDispatch(code, response.data.filter(lecture => (lecture.major_code === code)));
+        });
+      })
+      .catch((response) => {
+      });
+
+    if (majorOnly) {
+      return;
+    }
+
+    axios.get(`${BASE_URL}/api/lectures`, { params: {
+      year: year,
+      semester: semester,
+      group: 'Humanity',
+    } })
+      .then((response) => {
+        const newProps = this.props;
+        if (newProps.year !== year || newProps.semester !== semester) {
+          return;
+        }
+        setListLecturesDispatch('humanity', response.data);
+      })
+      .catch((response) => {
+      });
+
+    axios.post(`${BASE_URL}/api/timetable/wishlist_load`, {
+      year: year,
+      semester: semester,
+    })
+      .then((response) => {
+        const newProps = this.props;
+        if (newProps.year !== year || newProps.semester !== semester
+        ) {
+          return;
+        }
+        setListLecturesDispatch('cart', response.data);
+      })
+      .catch((response) => {
+      });
   }
 
   changeTab = (list) => {
@@ -79,6 +153,8 @@ class LectureListTabs extends Component {
 const mapStateToProps = state => ({
   user: state.common.user.user,
   currentList: state.timetable.list.currentList,
+  year: state.timetable.semester.year,
+  semester: state.timetable.semester.semester,
   search: state.timetable.list.search,
   major: state.timetable.list.major,
   humanity: state.timetable.list.humanity,
@@ -102,11 +178,22 @@ const mapDispatchToProps = dispatch => ({
   clearLectureActiveDispatch: () => {
     dispatch(clearLectureActive());
   },
+  setListLecturesDispatch: (code, lectures) => {
+    dispatch(setListLectures(code, lectures));
+  },
+  clearListsLecturesDispatch: () => {
+    dispatch(clearListsLectures());
+  },
+  setListMajorLecturesDispatch: (majorCode, lectures) => {
+    dispatch(setListMajorLectures(majorCode, lectures));
+  },
 });
 
 LectureListTabs.propTypes = {
   user: userShape,
   currentList: PropTypes.string.isRequired,
+  year: PropTypes.number,
+  semester: PropTypes.number,
   search: PropTypes.shape({
     courses: PropTypes.arrayOf(PropTypes.arrayOf(lectureShape)),
   }).isRequired,
@@ -125,6 +212,9 @@ LectureListTabs.propTypes = {
   setListMajorCodesDispatch: PropTypes.func.isRequired,
   setCurrentListDispatch: PropTypes.func.isRequired,
   clearLectureActiveDispatch: PropTypes.func.isRequired,
+  setListLecturesDispatch: PropTypes.func.isRequired,
+  clearListsLecturesDispatch: PropTypes.func.isRequired,
+  setListMajorLecturesDispatch: PropTypes.func.isRequired,
 };
 
 export default withTranslation()(connect(mapStateToProps, mapDispatchToProps)(LectureListTabs));
