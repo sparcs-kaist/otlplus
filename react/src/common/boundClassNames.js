b2973a3b99d15e68a053515f57f7884ad984ecf0
zealotzealot <hsh0908y@gmail.com>
2019-12-31 20:57:25 +0900

    Merge credit.module.css into App.module.scss

--- react/src/common/boundClassNames.js
+++ react/src/common/boundClassNames.js
@@ -2,12 +2,10 @@ import classNames from 'classnames/bind';
 
 import appStyles from '../styles/App.module.scss';
 import timetablStyles from '../styles/timetable.module.css';
-import creditStyles from '../styles/credit.module.css';
 import syllabusStyles from '../styles/syllabus.module.scss';
 import guidelineStyles from '../styles/guideline.module.scss';
 
 export const appBoundClassNames = classNames.bind(appStyles);
 export const timetableBoundClassNames = classNames.bind(timetablStyles);
-export const creditBoundClassNames = classNames.bind(creditStyles);
 export const syllabusBoundClassNames = classNames.bind(syllabusStyles);
 export const guidelineBoundClassNames = classNames.bind(guidelineStyles);
--- react/src/pages/CreditPage.js
+++ react/src/pages/CreditPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { creditBoundClassNames, appBoundClassNames as classNames } from '../common/boundClassNames';
+import { appBoundClassNames as classNames } from '../common/boundClassNames';
 
 import Footer from '../components/guideline/Footer';
 
@@ -9,12 +9,12 @@ class Button extends Component {
   render() {
     const { index, current, onClick, children } = this.props;
     const className = (index === current)
-      ? creditBoundClassNames('block', 'active')
-      : creditBoundClassNames('block');
+      ? classNames('block', 'block--project', 'active')
+      : classNames('block', 'block--project');
     return (
-      <div onClick={() => onClick(index)} className={className} key={index}>
+      <button onClick={() => onClick(index)} className={className} key={index}>
         {children}
-      </div>
+      </button>
     );
   }
 }
@@ -52,183 +52,183 @@ class CreditPage extends Component {
     switch (tab) {
       case 1:
         return (
-          <div className={creditBoundClassNames('content', 'tab1')}>
-            <h4>Project Manager</h4>
+          <div className={classNames('section-content--credit__people-list')}>
+            <div className={classNames('title')}>Project Manager</div>
 
-            <h4>Designer</h4>
+            <div className={classNames('title')}>Designer</div>
 
-            <h4>Developer</h4>
+            <div className={classNames('title')}>Developer</div>
           </div>
         );
       case 2:
         return (
-          <div className={creditBoundClassNames('content', 'tab2')}>
-            <h4>Developer</h4>
-            <div className={creditBoundClassNames('people')}>김민우</div>
-            <div className={creditBoundClassNames('people')}>김종균</div>
-            <div className={creditBoundClassNames('people')}>김준기</div>
-            <div className={creditBoundClassNames('people')}>유충국</div>
+          <div className={classNames('section-content--credit__people-list')}>
+            <div className={classNames('title')}>Developer</div>
+            <div className={classNames()}>김민우</div>
+            <div className={classNames()}>김종균</div>
+            <div className={classNames()}>김준기</div>
+            <div className={classNames()}>유충국</div>
 
-            <h4>Special Thanks To</h4>
-            <div className={creditBoundClassNames('people')}>강철</div>
-            <div className={creditBoundClassNames('people')}>안병욱</div>
+            <div className={classNames('title')}>Special Thanks To</div>
+            <div className={classNames()}>강철</div>
+            <div className={classNames()}>안병욱</div>
           </div>
         );
       case 3:
         return (
-          <div className={creditBoundClassNames('content', 'tab3')}>
-            <h4>Project Manager</h4>
-            <div className={creditBoundClassNames('people')}>
+          <div className={classNames('section-content--credit__people-list')}>
+            <div className={classNames('title')}>Project Manager</div>
+            <div className={classNames()}>
               배성경
-              <div>(2011)</div>
+              <div className={classNames('caption')}>(2011)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               김재겸
-              <div>(2012)</div>
+              <div className={classNames('caption')}>(2012)</div>
             </div>
 
-            <h4>Developer</h4>
-            <div className={creditBoundClassNames('people')}>김재겸</div>
-            <div className={creditBoundClassNames('people')}>박일우</div>
-            <div className={creditBoundClassNames('people')}>배성경</div>
-            <div className={creditBoundClassNames('people')}>안재만</div>
-            <div className={creditBoundClassNames('people')}>이윤석</div>
-            <div className={creditBoundClassNames('people')}>정재성</div>
-            <div className={creditBoundClassNames('people')}>조유정</div>
+            <div className={classNames('title')}>Developer</div>
+            <div className={classNames()}>김재겸</div>
+            <div className={classNames()}>박일우</div>
+            <div className={classNames()}>배성경</div>
+            <div className={classNames()}>안재만</div>
+            <div className={classNames()}>이윤석</div>
+            <div className={classNames()}>정재성</div>
+            <div className={classNames()}>조유정</div>
           </div>
         );
       case 4:
         return (
-          <div className={creditBoundClassNames('content', 'tab4')}>
-            <h4>Project Manager</h4>
-            <div className={creditBoundClassNames('people')}>
+          <div className={classNames('section-content--credit__people-list')}>
+            <div className={classNames('title')}>Project Manager</div>
+            <div className={classNames()}>
               유민정
-              <div>(2012.05 ~ 2012.09)</div>
+              <div className={classNames('caption')}>(2012.05 ~ 2012.09)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               마재의
-              <div>(2012.09 ~ 2013.03)</div>
+              <div className={classNames('caption')}>(2012.09 ~ 2013.03)</div>
             </div>
 
-            <h4>Designer</h4>
-            <div className={creditBoundClassNames('people')}>박지향</div>
+            <div className={classNames('title')}>Designer</div>
+            <div className={classNames()}>박지향</div>
 
-            <h4>Developer</h4>
-            <div className={creditBoundClassNames('people')}>김정민</div>
-            <div className={creditBoundClassNames('people')}>마재의</div>
-            <div className={creditBoundClassNames('people')}>박중언</div>
-            <div className={creditBoundClassNames('people')}>박지혁</div>
-            <div className={creditBoundClassNames('people')}>유민정</div>
-            <div className={creditBoundClassNames('people')}>윤필립</div>
-            <div className={creditBoundClassNames('people')}>이태현</div>
-            <div className={creditBoundClassNames('people')}>정종혁</div>
-            <div className={creditBoundClassNames('people')}>정창제</div>
-            <div className={creditBoundClassNames('people')}>채종욱</div>
+            <div className={classNames('title')}>Developer</div>
+            <div className={classNames()}>김정민</div>
+            <div className={classNames()}>마재의</div>
+            <div className={classNames()}>박중언</div>
+            <div className={classNames()}>박지혁</div>
+            <div className={classNames()}>유민정</div>
+            <div className={classNames()}>윤필립</div>
+            <div className={classNames()}>이태현</div>
+            <div className={classNames()}>정종혁</div>
+            <div className={classNames()}>정창제</div>
+            <div className={classNames()}>채종욱</div>
           </div>
         );
       case 5:
         return (
-          <div className={creditBoundClassNames('content', 'tab5')}>
-            <h4>Project Manager</h4>
-            <div className={creditBoundClassNames('people')}>
+          <div className={classNames('section-content--credit__people-list')}>
+            <div className={classNames('title')}>Project Manager</div>
+            <div className={classNames()}>
               황태현
-              <div>(2015.09 ~ 2015.11)</div>
+              <div className={classNames('caption')}>(2015.09 ~ 2015.11)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               서동민
-              <div>(2015.12 ~ 2016.06)</div>
+              <div className={classNames('caption')}>(2015.12 ~ 2016.06)</div>
             </div>
 
-            <h4>Designer</h4>
-            <div className={creditBoundClassNames('people')}>김찬욱</div>
+            <div className={classNames('title')}>Designer</div>
+            <div className={classNames()}>김찬욱</div>
 
-            <h4>Developer</h4>
-            <div className={creditBoundClassNames('people')}>고지훈</div>
-            <div className={creditBoundClassNames('people')}>김강인</div>
-            <div className={creditBoundClassNames('people')}>김재성</div>
-            <div className={creditBoundClassNames('people')}>서동민</div>
-            <div className={creditBoundClassNames('people')}>이강원</div>
-            <div className={creditBoundClassNames('people')}>조성원</div>
-            <div className={creditBoundClassNames('people')}>최정운</div>
-            <div className={creditBoundClassNames('people')}>한승현</div>
-            <div className={creditBoundClassNames('people')}>황태현</div>
+            <div className={classNames('title')}>Developer</div>
+            <div className={classNames()}>고지훈</div>
+            <div className={classNames()}>김강인</div>
+            <div className={classNames()}>김재성</div>
+            <div className={classNames()}>서동민</div>
+            <div className={classNames()}>이강원</div>
+            <div className={classNames()}>조성원</div>
+            <div className={classNames()}>최정운</div>
+            <div className={classNames()}>한승현</div>
+            <div className={classNames()}>황태현</div>
           </div>
         );
       case 6:
         return (
-          <div className={creditBoundClassNames('content', 'tab6')}>
-            <h4>Project Manager</h4>
-            <div className={creditBoundClassNames('people')}>
+          <div className={classNames('section-content--credit__people-list')}>
+            <div className={classNames('title')}>Project Manager</div>
+            <div className={classNames()}>
               김재성
-              <div>(2016.09 ~ 2017.05)</div>
+              <div className={classNames('caption')}>(2016.09 ~ 2017.05)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               한승현
-              <div>(2017.06 ~ )</div>
+              <div className={classNames('caption')}>(2017.06 ~ )</div>
             </div>
 
-            <h4>Designer</h4>
-            <div className={creditBoundClassNames('people')}>한승현</div>
+            <div className={classNames('title')}>Designer</div>
+            <div className={classNames()}>한승현</div>
 
-            <h4>Developer</h4>
-            <div className={creditBoundClassNames('people')}>고지훈</div>
-            <div className={creditBoundClassNames('people')}>김재성</div>
-            <div className={creditBoundClassNames('people')}>김태준</div>
-            <div className={creditBoundClassNames('people')}>서덕담</div>
-            <div className={creditBoundClassNames('people')}>오종훈</div>
-            <div className={creditBoundClassNames('people')}>이강원</div>
-            <div className={creditBoundClassNames('people')}>조형준</div>
-            <div className={creditBoundClassNames('people')}>최정운</div>
-            <div className={creditBoundClassNames('people')}>한승현</div>
+            <div className={classNames('title')}>Developer</div>
+            <div className={classNames()}>고지훈</div>
+            <div className={classNames()}>김재성</div>
+            <div className={classNames()}>김태준</div>
+            <div className={classNames()}>서덕담</div>
+            <div className={classNames()}>오종훈</div>
+            <div className={classNames()}>이강원</div>
+            <div className={classNames()}>조형준</div>
+            <div className={classNames()}>최정운</div>
+            <div className={classNames()}>한승현</div>
           </div>
         );
       case 7:
         return (
-          <div className={creditBoundClassNames('content', 'tab7')}>
-            <h4>LKIN</h4>
-            <div className={creditBoundClassNames('people')}>
+          <div className={classNames('section-content--credit__people-list')}>
+            <div className={classNames('title')}>LKIN</div>
+            <div className={classNames()}>
               서창민
-              <div>(2009 ~ 2010)</div>
+              <div className={classNames('caption')}>(2009 ~ 2010)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               이근홍
-              <div>(2011 ~ 2012)</div>
+              <div className={classNames('caption')}>(2011 ~ 2012)</div>
             </div>
 
-            <h4>OTL</h4>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames('title')}>OTL</div>
+            <div className={classNames()}>
               배성경
-              <div>(2011)</div>
+              <div className={classNames('caption')}>(2011)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               김재겸
-              <div>(2012)</div>
+              <div className={classNames('caption')}>(2012)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               유민정
-              <div>(2012.05 ~ 2012.09)</div>
+              <div className={classNames('caption')}>(2012.05 ~ 2012.09)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               마재의
-              <div>(2012.09 ~ 2015.04)</div>
+              <div className={classNames('caption')}>(2012.09 ~ 2015.04)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               황태현
-              <div>(2015.05 ~ 2015.11)</div>
+              <div className={classNames('caption')}>(2015.05 ~ 2015.11)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               서동민
-              <div>(2015.12 ~ 2016.05)</div>
+              <div className={classNames('caption')}>(2015.12 ~ 2016.05)</div>
             </div>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames()}>
               김재성
-              <div>(2016.06 ~ 2017.06)</div>
+              <div className={classNames('caption')}>(2016.06 ~ 2017.06)</div>
             </div>
 
-            <h4>OTL PLUS</h4>
-            <div className={creditBoundClassNames('people')}>
+            <div className={classNames('title')}>OTL PLUS</div>
+            <div className={classNames()}>
               한승현
-              <div>(2017.06 ~ )</div>
+              <div className={classNames('caption')}>(2017.06 ~ )</div>
             </div>
           </div>
         );
@@ -247,7 +247,7 @@ class CreditPage extends Component {
           <div className={classNames('section')}>
             {/* eslint-disable-next-line react/jsx-indent */}
                 <div className={classNames('section-content', 'section-content--credit')}>
-                  <div className={creditBoundClassNames('block_wrap')}>
+                  <div className={classNames('section-content--credit__blocks')}>
                     <Button index={1} onClick={this.changePage} current={currentTab}>
                       LKIN
                     </Button>
@@ -279,15 +279,8 @@ class CreditPage extends Component {
                     <Button index={7} onClick={this.changePage} current={currentTab}>
                       SYSOP
                     </Button>
-                    <div className={creditBoundClassNames('dummy')} />
-                    <div className={creditBoundClassNames('dummy')} />
-                    <div className={creditBoundClassNames('dummy')} />
-                    <div className={creditBoundClassNames('dummy')} />
-                    <div className={creditBoundClassNames('dummy')} />
-                    <div className={creditBoundClassNames('dummy')} />
                   </div>
 
-                  <h1>Credit</h1>
                   {this._getContent(currentTab)}
                 </div>
           </div>
