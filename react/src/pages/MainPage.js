2b70f8b78a4e60185cd30be82d13e3ce6828fa61
zealotzealot <hsh0908y@gmail.com>
2020-01-03 20:17:13 +0900

    Implement fetching feeds from setver in MainPage

--- react/src/pages/MainPage.js
+++ react/src/pages/MainPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import { appBoundClassNames as classNames } from '../common/boundClassNames';
+import axios from '../common/presetAxios';
+import { BASE_URL } from '../common/constants';
 
 import Footer from '../components/guideline/Footer';
 
@@ -14,7 +16,46 @@ import MainSearchSection from '../components/sections/main/MainSearchSection';
 
 
 class MainPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      feedDays: [],
+    };
+  }
+
+
+  componentDidMount() {
+    const date = new Date().toJSON().slice(0, 10);
+    this._fetchFeeds(date);
+  }
+
+
+  _fetchFeeds = (date) => {
+    const { feedDays } = this.state;
+
+    axios.get(`${BASE_URL}/api/feeds`, { params: {
+      date: date,
+    } })
+      .then((response) => {
+        this.setState({
+          feedDays: [
+            ...feedDays,
+            {
+              date: date,
+              feeds: response.data,
+            },
+          ],
+        });
+      })
+      .catch((response) => {
+      });
+  }
+
+
   render() {
+    const { feedDays } = this.state;
+
     return (
       <>
         <section className={classNames('main-image')}>
@@ -36,29 +77,59 @@ class MainPage extends Component {
               <AcademicScheduleSection />
             </div>
           </div>
-          <div className={classNames('main-date')}>
-            오늘
-          </div>
+          {
+            feedDays.map(d => (
+              <React.Fragment key={d.date}>
+                <div className={classNames('main-date')}>
+                  {d.date}
+                </div>
+                {d.feeds.map((f) => {
+                  if (f.type === 'REVIEW_WRITE') {
+                    return (
+                    /* eslint-disable-next-line react/jsx-indent */
           <div className={classNames('section-wrap')}>
             <div className={classNames('section')}>
               <ReviewWriteSection />
             </div>
           </div>
+                    );
+                  }
+                  if (f.type === 'RELATED_COURSE') {
+                    return (
+                    /* eslint-disable-next-line react/jsx-indent */
           <div className={classNames('section-wrap')}>
             <div className={classNames('section')}>
               <RelatedCourseSection />
             </div>
           </div>
+                    );
+                  }
+                  if (f.type === 'LATEST_REVIEW') {
+                    return (
+                    /* eslint-disable-next-line react/jsx-indent */
           <div className={classNames('section-wrap')}>
             <div className={classNames('section')}>
               <LatestReviewSection />
             </div>
           </div>
+                    );
+                  }
+                  if (f.type === 'FAMOUS_REVIEW') {
+                    return (
+                    /* eslint-disable-next-line react/jsx-indent */
           <div className={classNames('section-wrap')}>
             <div className={classNames('section')}>
-              <FamousReviewSection />
+              <FamousReviewSection department={f.department} reviews={f.reviews} />
             </div>
           </div>
+                    );
+                  }
+                  return null;
+                })
+                }
+              </React.Fragment>
+            ))
+          }
         </section>
         <Footer />
       </>
