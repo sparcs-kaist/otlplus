a99fb3eec9c0570214d7000bde467d63046e3dd1
zealotzealot <hsh0908y@gmail.com>
2020-01-03 22:31:13 +0900

    Add date string to MainPage

--- react/src/common/i18n.js
+++ react/src/common/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import moment from 'moment';
 
 import en from '../translations/translation.en';
 import ko from '../translations/translation.ko';
@@ -26,6 +27,13 @@ i18n
     },
     interpolation: {
       escapeValue: false,
+      formatSeparator: ',',
+      format: (value, formatting, lng) => {
+        if (value instanceof Date) {
+          return moment(value).locale(lng).format(formatting);
+        }
+        return value.toString();
+      },
     },
   });
 
