c6a9ff81573b7bd334da76fb587fcf9c3da7b0a1
zealotzealot <hsh0908y@gmail.com>
2020-01-03 21:31:48 +0900

    Implement ReviewWriteDailyUserFeed and RelatedCourseDailyUserFeed

--- apps/main/views.py
+++ apps/main/views.py
@@ -4,10 +4,11 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_http_methods, require_POST
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
+from utils.decorators import login_required_ajax
 
 from apps.session.models import UserProfile
 from apps.subject.models import Course, Department
-from apps.main.models import RandomCourseReco, FamousReviewDailyFeed
+from apps.main.models import RandomCourseReco, FamousReviewDailyFeed, ReviewWriteDailyUserFeed, RelatedCourseDailyUserFeed
 
 from apps.timetable.views import _lecture_to_dict
 
@@ -19,15 +20,21 @@ import json
 from datetime import date
 
 
+@login_required_ajax
 @require_http_methods(['GET'])
 def feeds_list_view(request):
     if request.method == 'GET':
         date = request.GET.get('date', None)
+        user = UserProfile.objects.get(user=request.user)
 
         department = Department.objects.filter(code="ID").first()
         famous_review_daily_feed = FamousReviewDailyFeed.get(date=date, department=department)
 
-        feeds = sorted([famous_review_daily_feed], key=(lambda f: f.priority))
+        review_write_daily_user_feed = ReviewWriteDailyUserFeed.get(date=date, user=user)
+
+        related_course_daily_user_feed = RelatedCourseDailyUserFeed.get(date=date, user=user)
+
+        feeds = sorted([famous_review_daily_feed, review_write_daily_user_feed, related_course_daily_user_feed], key=(lambda f: f.priority))
         result = [f.toJson() for f in feeds]
         return JsonResponse(result, safe=False)
 
