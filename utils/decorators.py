899d87c31517071144357fd13924720ed72bc87b
zealotzealot <hsh0908y@gmail.com>
2020-01-04 17:24:27 +0900

    Change login_required_ajax to return 401 instead of 403

--- utils/decorators.py
+++ utils/decorators.py
@@ -12,7 +12,8 @@ def login_required_ajax(function=None, response_content=u'You have to log in fir
         def handler(request, *args, **kwargs):
             if request.user.is_authenticated():
                 return view_func(request, *args, **kwargs)
-            return HttpResponseForbidden(response_content)
+            print(401)
+            return HttpResponse(status=401)
         return handler
 
     return decorate(function)
