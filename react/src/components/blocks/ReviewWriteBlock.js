b581f5a9d3a6eb143002dbf632856fd13e2c051c
zealotzealot <hsh0908y@gmail.com>
2020-01-05 16:26:17 +0900

    Implement setting data in review write

--- react/src/components/blocks/ReviewWriteBlock.js
+++ react/src/components/blocks/ReviewWriteBlock.js
@@ -11,11 +11,42 @@ import lectureShape from '../../shapes/NestedLectureShape';
 // eslint-disable-next-line arrow-body-style
 const ReviewWriteBlock = ({ t, lecture }) => {
   const [isUploading, setIsUploading] = useState(false);
+  const [content, setContent] = useState('');
+  const [grade, setGrade] = useState(undefined);
+  const [load, setLoad] = useState(undefined);
+  const [speech, setSpeech] = useState(undefined);
+
+  const onContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onScoreChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'grade') {
+      setGrade(value);
+    }
+    else if (name === 'load') {
+      setLoad(value);
+    }
+    else if (name === 'speech') {
+      setSpeech(value);
+    }
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
     e.stopPropagation();
 
+    if (content.length === 0) {
+      // eslint-disable-next-line no-alert
+      alert(t('ui.message.emptyContent'));
+      return;
+    }
+    if ((grade === undefined) || (load === undefined) || (speech === undefined)) {
+      // eslint-disable-next-line no-alert
+      alert(t('ui.message.scoreNotSelected'));
+      return;
+    }
     if (isUploading) {
       // eslint-disable-next-line no-alert
       alert(t('ui.message.alreadyUploading'));
@@ -24,10 +55,10 @@ const ReviewWriteBlock = ({ t, lecture }) => {
 
     setIsUploading(true);
     axios.post(`${BASE_URL}/api/review/insert/${lecture.id}`, {
-      content: 'Sample Content',
-      gradescore: 4,
-      speechscore: 4,
-      loadscore: 4,
+      content: content,
+      gradescore: grade,
+      speechscore: speech,
+      loadscore: load,
     })
       .then((response) => {
         setIsUploading(false);
@@ -43,74 +74,74 @@ const ReviewWriteBlock = ({ t, lecture }) => {
         <span>{lecture[t('js.property.professors_str_short')]}</span>
         <span>{`${lecture.year} ${[undefined, t('ui.semester.spring'), t('ui.semester.summer'), t('ui.semester.fall'), t('ui.semester.winter')][lecture.semester]}`}</span>
       </div>
-      <textarea className={classNames('block--review-write__content')} placeholder={t('ui.placeholder.reviewContent')} />
+      <textarea className={classNames('block--review-write__content')} placeholder={t('ui.placeholder.reviewContent')} value={content} onChange={onContentChange} />
       <div>
         <div className={classNames('block--review-write__score')}>
           <span className={classNames('block--review-write__score__name')}>{t('ui.score.grade')}</span>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="grade" value="A" />
+            <input type="radio" name="grade" value="5" onChange={onScoreChange} />
             <span>A</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="grade" value="B" />
+            <input type="radio" name="grade" value="4" onChange={onScoreChange} />
             <span>B</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="grade" value="C" />
+            <input type="radio" name="grade" value="3" onChange={onScoreChange} />
             <span>C</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="grade" value="D" />
+            <input type="radio" name="grade" value="2" onChange={onScoreChange} />
             <span>D</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="grade" value="F" />
+            <input type="radio" name="grade" value="1" onChange={onScoreChange} />
             <span>F</span>
           </label>
         </div>
         <div className={classNames('block--review-write__score')}>
           <span className={classNames('block--review-write__score__name')}>{t('ui.score.load')}</span>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="load" value="A" />
+            <input type="radio" name="load" value="5" onChange={onScoreChange} />
             <span>A</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="load" value="B" />
+            <input type="radio" name="load" value="4" onChange={onScoreChange} />
             <span>B</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="load" value="C" />
+            <input type="radio" name="load" value="3" onChange={onScoreChange} />
             <span>C</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="load" value="D" />
+            <input type="radio" name="load" value="2" onChange={onScoreChange} />
             <span>D</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="load" value="F" />
+            <input type="radio" name="load" value="1" onChange={onScoreChange} />
             <span>F</span>
           </label>
         </div>
         <div className={classNames('block--review-write__score')}>
           <span className={classNames('block--review-write__score__name')}>{t('ui.score.speech')}</span>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="speech" value="B" />
-            <span>B</span>
-          </label>
-          <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="speech" value="C" />
-            <span>C</span>
-          </label>
-          <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="speech" value="A" />
+            <input type="radio" name="speech" value="5" onChange={onScoreChange} />
             <span>A</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="speech" value="D" />
+            <input type="radio" name="speech" value="4" onChange={onScoreChange} />
+            <span>B</span>
+          </label>
+          <label className={classNames('block--review-write__score__option')}>
+            <input type="radio" name="speech" value="3" onChange={onScoreChange} />
+            <span>C</span>
+          </label>
+          <label className={classNames('block--review-write__score__option')}>
+            <input type="radio" name="speech" value="2" onChange={onScoreChange} />
             <span>D</span>
           </label>
           <label className={classNames('block--review-write__score__option')}>
-            <input type="radio" name="speech" value="F" />
+            <input type="radio" name="speech" value="1" onChange={onScoreChange} />
             <span>F</span>
           </label>
         </div>
