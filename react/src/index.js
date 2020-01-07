156b8a2a6d270a7557a4dc0698fe10588e721cb7
zealotzealot <hsh0908y@gmail.com>
2020-01-08 06:05:32 +0900

    Inject Google Analytics in React

--- react/src/index.js
+++ react/src/index.js
@@ -7,9 +7,19 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
+
+const history = createBrowserHistory();
+
+const trackingId = 'UA-144615112-2';
+ReactGA.initialize(trackingId);
+history.listen(location => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+});
 
 ReactDOM.render(
-    <Router history={createBrowserHistory()}>
+    <Router history={history}>
         <App/>
     </Router>,
     document.getElementById('root'));
