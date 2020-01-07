2dbc4eee9cef8f573533c8292eb3302a1aa078a2
zealotzealot <hsh0908y@gmail.com>
2020-01-07 22:04:30 +0900

    Implement TakenLecturesSection

--- react/src/pages/WriteReviewsPage.js
+++ react/src/pages/WriteReviewsPage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { appBoundClassNames as classNames } from '../common/boundClassNames';
+import TakenLecturesSection from '../components/sections/write-reviews/TakenLecturesSection';
 
 
 class WriteReviewsPage extends Component {
@@ -12,10 +13,10 @@ class WriteReviewsPage extends Component {
         <section className={classNames('content', 'content--no-scroll')}>
           <div className={classNames('section-wrap', 'section-wrap--desktop-1v3--left', 'section-wrap--mobile-full')}>
             <div className={classNames('section')}>
-              123
+              <TakenLecturesSection />
             </div>
           </div>
-          <div className={classNames('section-wrap', 'section-wrap--desktop-1v3--right')}>
+          <div className={classNames('section-wrap', 'section-wrap--desktop-1v3--right', 'mobile-modal', (false ? '' : 'mobile-hidden'))}>
             <div className={classNames('section')}>
               123
             </div>
