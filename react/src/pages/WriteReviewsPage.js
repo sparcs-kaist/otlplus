446784f9d2377286991e758061289404e3d869ff
zealotzealot <hsh0908y@gmail.com>
2020-01-07 23:35:43 +0900

    Implement LatestReviewsSubSection

--- react/src/pages/WriteReviewsPage.js
+++ react/src/pages/WriteReviewsPage.js
@@ -2,12 +2,56 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import axios from '../common/presetAxios';
+import { BASE_URL } from '../common/constants';
 import { appBoundClassNames as classNames } from '../common/boundClassNames';
+
 import TakenLecturesSection from '../components/sections/write-reviews/TakenLecturesSection';
 import ReviewWriteSubSection from '../components/sections/write-reviews/ReviewWriteSubSection';
+import LatestReviewsSubSection from '../components/sections/write-reviews/LatestReviewsSubSection';
+import Scroller from '../components/Scroller';
+import { addReviews } from '../actions/write-reviews/latestReviews';
 
 
 class WriteReviewsPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+
+  componentDidMount() {
+    this._fetchReviews();
+  }
+
+
+  _fetchReviews = () => {
+    const { addReviewsDispatch } = this.props;
+    const { loading } = this.state;
+
+    if (loading) {
+      return;
+    }
+
+    this.setState({
+      loading: true,
+    });
+    axios.get(`${BASE_URL}/api/review/latest/0`, {
+    })
+      .then((response) => {
+        this.setState({
+          loading: false,
+        });
+        addReviewsDispatch(response.data);
+      })
+      .catch((error) => {
+      });
+  }
+
+
   render() {
     return (
       <>
@@ -19,7 +63,12 @@ class WriteReviewsPage extends Component {
           </div>
           <div className={classNames('section-wrap', 'section-wrap--desktop-1v3--right', 'mobile-modal', (false ? '' : 'mobile-hidden'))}>
             <div className={classNames('section')}>
-              <ReviewWriteSubSection />
+              <div className={classNames('section-content')}>
+                <Scroller>
+                  <ReviewWriteSubSection />
+                  <LatestReviewsSubSection />
+                </Scroller>
+              </div>
             </div>
           </div>
         </section>
@@ -32,9 +81,13 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
+  addReviewsDispatch: (reviews) => {
+    dispatch(addReviews(reviews));
+  },
 });
 
 WriteReviewsPage.propTypes = {
+  addReviewsDispatch: PropTypes.func.isRequired,
 };
 
 
--- react/src/reducers/write-reviews/index.js
+++ react/src/reducers/write-reviews/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import lectureSelected from './lectureSelected';
+import latestReviews from './latestReviews';
 
 const CombinedReducer = combineReducers({
   lectureSelected: lectureSelected,
+  latestReviews: latestReviews,
 });
 
 export default CombinedReducer;
