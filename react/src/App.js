deeb8f53738cf0937057d21d08b69bbb0c0a261f
zealotzealot <hsh0908y@gmail.com>
2020-01-12 21:32:16 +0900

    Fix types mismatching while query string converting

--- react/src/App.js
+++ react/src/App.js
@@ -51,6 +51,36 @@ class App extends Component {
   }
 
   render() {
+    const parseObject = (object) => {
+      if (typeof object === 'object') {
+        return Object.entries(object)
+          .map(([k, v]) => [k, parseObject(v)])
+          .reduce((acc, val) => Object.assign({}, acc, { [val[0]]: val[1] }), {});
+      }
+
+      if (/^[0-9]+$/.test(object)) {
+        return parseInt(object, 10);
+      }
+
+      const keywords = {
+        true: true,
+        false: false,
+        null: null,
+        undefined: undefined,
+      };
+      if (object in keywords) {
+        return keywords[object];
+      }
+
+      return object;
+    };
+
+    const parseQueryString = (search) => {
+      const qsParsed = qs.parse(search, { ignoreQueryPrefix: true });
+
+      return parseObject(qsParsed);
+    };
+
     return (
       <Provider store={store}>
         <>
@@ -61,7 +91,7 @@ class App extends Component {
               path="/dictionary"
               render={props => (
                 props.location.search
-                  ? <Redirect to={{ ...props.location, state: { ...props.location.state, ...qs.parse(props.location.search, { ignoreQueryPrefix: true }) }, search: '' }} />
+                  ? <Redirect to={{ ...props.location, state: { ...props.location.state, ...parseQueryString(props.location.search) }, search: '' }} />
                   : <DictionaryPage {...props} />
               )}
             />
@@ -70,7 +100,7 @@ class App extends Component {
               path="/timetable"
               render={props => (
                 props.location.search
-                  ? <Redirect to={{ ...props.location, state: { ...props.location.state, ...qs.parse(props.location.search, { ignoreQueryPrefix: true }) }, search: '' }} />
+                  ? <Redirect to={{ ...props.location, state: { ...props.location.state, ...parseQueryString(props.location.search) }, search: '' }} />
                   : <TimetablePage {...props} />
               )}
             />
