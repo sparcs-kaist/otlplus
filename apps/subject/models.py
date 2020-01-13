758d5e0e152c9713bc6e62b45e2ea43462264d85
zealotzealot <hsh0908y@gmail.com>
2020-01-14 03:32:59 +0900

    Refactor addUserspecificData() in Course toJson()

--- apps/subject/models.py
+++ apps/subject/models.py
@@ -561,13 +561,19 @@ class Course(models.Model):
             # Add user read info
             if (not user) or (not user.is_authenticated()):
                 is_read = False
-            elif not self.read_users_courseuser.filter(user_profile__user=user).exists():
-                is_read = False
-            elif self.latest_written_datetime is None:
-                is_read = True
             else:
-                course_user = self.read_users_courseuser.get(user_profile__user=user)
-                is_read = self.latest_written_datetime < course_user.latest_read_datetime
+                try:
+                    course_user = self.read_users_courseuser.get(user_profile__user=user)
+                except CourseUser.DoesNotExist:
+                    course_user = None
+
+                if course_user is None:
+                    is_read = False
+                elif self.latest_written_datetime is None:
+                    is_read = True
+                else:
+                    is_read = self.latest_written_datetime < course_user.latest_read_datetime
+
             result.update({
                 'userspecific_is_read': is_read,
             })
