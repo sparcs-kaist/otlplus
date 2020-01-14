55934592d58369ddc3c537ab228426b43fe56e0d
zealotzealot <hsh0908y@gmail.com>
2020-01-15 03:55:00 +0900

    Update script directory

--- db_update_script.sh
+++ db_update_script.sh
@@ -2,9 +2,9 @@
 echo "Start!"
 echo $(date +'%Y-%m-%d / %T') >db_update_script.log 2>&1
 BASEDIR=$(dirname $(readlink -f "$0"))
-python $BASEDIR/update_scholardb.py >>db_update_script.log 2>&1
+python $BASEDIR/do_import_scholardb.py >>db_update_script.log 2>&1
 python $BASEDIR/manage.py update_ProfessorCourseList >>db_update_script.log 2>&1
 python $BASEDIR/manage.py update_CourseFiltered >>db_update_script.log 2>&1
-python $BASEDIR/update_taken_lecture.py >>db_update_script.log 2>&1
+python $BASEDIR/do_import_taken_lecture.py >>db_update_script.log 2>&1
 
 echo "Done!"
