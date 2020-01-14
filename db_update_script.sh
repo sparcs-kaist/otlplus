fe8f36f748699528a04bbce92502db5d78381c89
zealotzealot <hsh0908y@gmail.com>
2020-01-15 03:49:36 +0900

    Remove migration in db_update_script.sh

--- db_update_script.sh
+++ db_update_script.sh
@@ -2,13 +2,6 @@
 echo "Start!"
 echo $(date +'%Y-%m-%d / %T') >db_update_script.log 2>&1
 BASEDIR=$(dirname $(readlink -f "$0"))
-python $BASEDIR/manage.py makemigrations >>db_update_script.log 2>&1
-python $BASEDIR/manage.py makemigrations review >>db_update_script.log 2>&1
-python $BASEDIR/manage.py makemigrations session >>db_update_script.log 2>&1
-python $BASEDIR/manage.py makemigrations timetable >>db_update_script.log 2>&1
-python $BASEDIR/manage.py makemigrations subject >>db_update_script.log 2>&1
-python $BASEDIR/manage.py check >>db_update_script.log 2>&1
-python $BASEDIR/manage.py migrate >>db_update_script.log 2>&1
 python $BASEDIR/update_scholardb.py >>db_update_script.log 2>&1
 python $BASEDIR/manage.py update_ProfessorCourseList >>db_update_script.log 2>&1
 python $BASEDIR/manage.py update_CourseFiltered >>db_update_script.log 2>&1
