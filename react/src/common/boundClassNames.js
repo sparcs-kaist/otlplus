9718800d72a9690fc15093a67c4e571fdf8e87a4
zealotzealot <hsh0908y@gmail.com>
2020-01-17 01:38:17 +0900

    Delete legacy timetable.module.css

--- react/src/common/boundClassNames.js
+++ react/src/common/boundClassNames.js
@@ -1,9 +1,7 @@
 import classNames from 'classnames/bind';
 
 import appStyles from '../styles/App.module.scss';
-import timetablStyles from '../styles/timetable.module.css';
 import guidelineStyles from '../styles/guideline.module.scss';
 
 export const appBoundClassNames = classNames.bind(appStyles);
-export const timetableBoundClassNames = classNames.bind(timetablStyles);
 export const guidelineBoundClassNames = classNames.bind(guidelineStyles);
