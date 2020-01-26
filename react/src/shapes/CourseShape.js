99ee2b6d699e1bf1bf1515ceca4e12aaea4de2ff
zealotzealot <hsh0908y@gmail.com>
2020-01-26 15:30:49 +0900

    Changed level filtering from extra field code_num to code regex matching

--- react/src/shapes/CourseShape.js
+++ react/src/shapes/CourseShape.js
@@ -9,7 +9,6 @@ const courseShape = PropTypes.shape({
   id: PropTypes.number.isRequired,
   old_code: PropTypes.string.isRequired,
   department: departmentShape,
-  code_num: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   type_en: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
