591143bfc18f5682670c43bca1b0980b4a2a4d01
zealotzealot <hsh0908y@gmail.com>
2020-09-05 00:31:52 +0900

    Add pagination to Lecture and Course list GET

--- apps/common/util.py
+++ apps/common/util.py
@@ -1,3 +1,30 @@
 
 def rgetattr(object_, names, default):
     return reduce(lambda o, n: getattr(o, n, default), names, object_)
+
+
+def getint(querydict, key, default=None):
+    value = querydict.get(key)
+    if value is None:
+        return default
+    else:
+        return int(value)
+
+
+def paginate_queryset(queryset, offset, limit, max_limit=150):
+    if offset is None:
+        real_offest = 0
+    elif offset >= 0:
+        real_offest = offset
+    else:
+        raise ValueError
+    
+    if limit is None:
+        real_limit = max_limit
+    elif 0 <= limit <= max_limit:
+        real_limit = limit
+    else:
+        raise ValueError
+
+    return queryset[real_offest : real_offest + real_limit]
+    
