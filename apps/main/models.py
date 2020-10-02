73a52e2b6bccad03a727e0f0977e0a19d2700cc1
zealotzealot <hsh0908y@gmail.com>
2020-10-02 22:33:26 +0900

    Remove unused django model RandomCourseReco

--- apps/main/models.py
+++ apps/main/models.py
@@ -8,15 +8,6 @@ import random
 
 # Create your models here.
 
-class RandomCourseReco(models.Model):
-    userprofile = models.ForeignKey(UserProfile, related_name="random_course_reco", on_delete=models.SET_NULL, null=True)
-    reco_date = models.DateField()
-    lecture = models.ForeignKey(Lecture, on_delete=models.PROTECT, db_index=True)
-
-    def __unicode__(self):
-        return u"User: %s, RecoDate: %s, Lecture: %s" % (self.userprofile, self.reco_date, self.lecture)
-
-
 class DailyFeed(models.Model):
     date = models.DateField()
     priority = models.FloatField()
