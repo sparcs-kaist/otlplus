33c32b7a876aa3bec78177a1cb514aae01637200
zealotzealot <hsh0908y@gmail.com>
2020-10-08 23:12:06 +0900

    Move model Notice and related view into new app 'support'

--- apps/support/urls.py
+++ apps/support/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
-from django.http import HttpResponseRedirect
 from . import views
 
 
 urlpatterns = [
-    url(r'^users/(?P<user_id>\d+)/feeds$', views.user_instance_feeds_view),
+    url(r'^notices$', views.notices_view),
 ]
--- apps/support/views.py
+++ apps/support/views.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.views.decorators.http import require_http_methods, require_POST
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from .models import Notice
+
+
 # Create your views here.
+
+@require_http_methods(['GET'])
+def notices_view(request):
+    if request.method == 'GET':
+        notices = Notice.objects.all()
+
+        time = request.GET.get('time', None)
+        if time:
+            notices = notices.filter(start_time__lte=time, end_time__gte=time)
+        
+        result = [n.toJson() for n in notices]
+        return JsonResponse(result, safe=False)
