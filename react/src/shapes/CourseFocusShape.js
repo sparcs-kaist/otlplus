9d7f54d3d3227c43d7de428fec14a93ab73ab5c6
zealotzealot <hsh0908y@gmail.com>
2021-10-20 22:32:47 +0900

    Fix missing fields in CourseFocusShape and LectureFocusShape

--- react/src/shapes/CourseFocusShape.js
+++ react/src/shapes/CourseFocusShape.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
 
 import courseShape from './CourseShape';
+import lectureShape from './LectureShape';
+import reviewShape from './ReviewShape';
 
 
 const courseFocusShape = PropTypes.shape({
   course: courseShape,
+  reviews: PropTypes.arrayOf(reviewShape),
+  lectures: PropTypes.arrayOf(lectureShape),
 });
 
 export default courseFocusShape;
--- react/src/shapes/LectureFocusShape.js
+++ react/src/shapes/LectureFocusShape.js
@@ -2,12 +2,14 @@ import PropTypes from 'prop-types';
 
 import { LectureFocusFrom } from '../reducers/timetable/lectureFocus';
 import lectureShape from './LectureShape';
+import reviewShape from './ReviewShape';
 
 
 const lectureFocusShape = PropTypes.shape({
   from: PropTypes.oneOf(Object.values(LectureFocusFrom)).isRequired,
   clicked: PropTypes.bool.isRequired,
   lecture: lectureShape,
+  reviews: PropTypes.arrayOf(reviewShape),
   multipleTitle: PropTypes.string.isRequired,
   multipleDetails: PropTypes.arrayOf(
     PropTypes.shape({
