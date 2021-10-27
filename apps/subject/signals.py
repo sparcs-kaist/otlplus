0b655535fe46c8349c77818c1ad18e6e1add31b6
zealotzealot <hsh0908y@gmail.com>
2021-10-27 23:03:56 +0900

    Remove unnecessary update_class_title() call in signals

--- apps/subject/signals.py
+++ apps/subject/signals.py
@@ -21,16 +21,24 @@ def lecture_professors_changed(**kwargs):
         kwargs["instance"].recalc_score()
 
 
+def _check_class_title_update_required(lecture):
+    is_title_equal = lecture.title in [lecture.common_title + lecture.class_title, lecture.common_title]
+    is_title_en_equal = lecture.title_en in [lecture.common_title_en + lecture.class_title_en, lecture.common_title_en]
+    return not (is_title_equal and is_title_en_equal)
+
+
 @receiver(post_save, sender=Lecture)
 def lecture_saved(**kwargs):
     update_fields = kwargs["update_fields"]
     if update_fields is None:
-        kwargs["instance"].update_class_title()
+        if _check_class_title_update_required(kwargs["instance"]):
+            kwargs["instance"].update_class_title()
     elif "common_title" not in update_fields \
          and "class_title" not in update_fields \
          and "common_title_en" not in update_fields \
          and "class_title_en" not in update_fields:
-        kwargs["instance"].update_class_title()
+        if _check_class_title_update_required(kwargs["instance"]):
+            kwargs["instance"].update_class_title()
     else:
         pass
     if not kwargs["created"]:
