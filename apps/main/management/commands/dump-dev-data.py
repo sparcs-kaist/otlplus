0a3c663074894d3ea9355a06df4f51b549246ca7
zealotzealot <hsh0908y@gmail.com>
2021-10-27 22:17:15 +0900

    Add info file to dump-dev-data output

--- apps/main/management/commands/dump-dev-data.py
+++ apps/main/management/commands/dump-dev-data.py
@@ -3,6 +3,7 @@ import json
 import random
 from typing import List, Dict
 
+from django.conf import settings
 from django.core import management
 from django.core.management.base import BaseCommand
 
@@ -36,14 +37,23 @@ class Command(BaseCommand):
         today = datetime.date.today()
         date_str = today.strftime('%y%m%d')
 
+        info_filename = f'{target_dir}/otldump_{date_str}_info.json'
         subject_filename = f'{target_dir}/otldump_{date_str}_subject.json'
+        review_filename = f'{target_dir}/otldump_{date_str}_review.json'
+
+        info_json = {
+            'date': today.isoformat(),
+            'version': settings.VERSION,
+            'files': [subject_filename, review_filename],
+        }
+        json.dump(info_json, open(info_filename, 'w'), indent=INDENT)
+
         management.call_command('dumpdata',
                                 'subject.Semester',
                                 'subject.Lecture', 'subject.ExamTime', 'subject.ClassTime',
                                 'subject.Department', 'subject.Course', 'subject.Professor',
                                 indent=INDENT, output=subject_filename)
 
-        review_filename = f'{target_dir}/otldump_{date_str}_review.json'
         management.call_command('dumpdata',
                                 'review.Review', 'review.ReviewVote',
                                 'review.MajorBestReview', 'review.HumanityBestReview',
