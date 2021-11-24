20be554ba18dba3e0df24522b5df4a15e88da64e
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2021-11-25 00:14:13 +0900

    개인정보취급방침 페이지 작성

--- react/src/common/guideline/components/Footer.js
+++ react/src/common/guideline/components/Footer.js
@@ -35,6 +35,11 @@ class Footer extends Component {
                   {t('ui.menu.licences')}
                 </Link>
               </span>
+              <span>
+                <Link to="/privacy">
+                  {t('ui.menu.privacy')}
+                </Link>
+              </span>
             </div>
           </div>
           <div className={classNames('contact')}>
