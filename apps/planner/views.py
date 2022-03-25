30b55738b8a2730d71d61a465ab7cdfa050826c6
GrasshopperBears <starday456@kaist.ac.kr>
2022-03-26 04:27:10 +0900

    basic template to handle planner item

--- apps/planner/views.py
+++ apps/planner/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
 
-# Create your views here.
+from utils.decorators import login_required_ajax
+from utils.util import ParseType, parse_body
+
+
+@method_decorator(login_required_ajax, name="dispatch")
+class UserInstancePlannerInstanceAddPlannerItemView(View):
+    def post(self, request, user_id, planner_id):
+        return JsonResponse()
+
+@method_decorator(login_required_ajax, name="dispatch")
+class UserInstancePlannerInstancePlannerItemView(View):
+    def patch(self, request, user_id, planner_id, planner_item_id):
+        return JsonResponse()
+    
+    def delete(self, request, user_id, planner_id, planner_item_id):
+        return JsonResponse()
