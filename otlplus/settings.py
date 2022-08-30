39c372202617eabef8a270853558309d2720b442
zealotzealot <hsh0908y@gmail.com>
2022-08-30 16:26:10 +0900

    Update otl version number to 3.2.4.5

--- otlplus/settings.py
+++ otlplus/settings.py
@@ -145,7 +145,7 @@ SSO_IS_BETA = DEBUG
 LOGIN_URL = "/session/login/"
 LOGOUT_URL = "/session/logout/"
 
-VERSION = "3.2.4.4"
+VERSION = "3.2.4.5"
 
 try:
     from settings_local import * # pylint: disable=wildcard-import, unused-wildcard-import
