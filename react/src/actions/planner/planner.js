d453fd5029dc8a0e002a1a111772528cb43ed5e2
zealotzealot <hsh0908y@gmail.com>
2022-11-28 18:10:04 +0900

    Update planner and implement reducer for planner

--- react/src/actions/planner/planner.js
+++ react/src/actions/planner/planner.js
@@ -4,16 +4,14 @@ const BASE_STRING = 'P_P_';
 export const RESET = BASE_STRING + 'RESET';
 export const SET_PLANNERS = BASE_STRING + 'SET_PLANNERS';
 export const CLEAR_PLANNERS = BASE_STRING + 'CLEAR_PLANNERS';
-export const SET_MY_PLANNER_LECTURES = BASE_STRING + 'SET_MY_PLANNER_LECTURES';
 export const SET_SELECTED_PLANNER = BASE_STRING + 'SET_SELECTED_PLANNER';
 export const CREATE_PLANNER = BASE_STRING + 'CREATE_PLANNER';
 export const DELETE_PLANNER = BASE_STRING + 'DELETE_PLANNER';
 export const DUPLICATE_PLANNER = BASE_STRING + 'DUPLICATE_PLANNER';
-export const ADD_LECTURE_TO_PLANNER = BASE_STRING + 'ADD_LECTURE_TO_PLANNER';
-export const REMOVE_LECTURE_FROM_PLANNER = BASE_STRING + 'REMOVE_LECTURE_FROM_PLANNER';
+// export const ADD_LECTURE_TO_PLANNER = BASE_STRING + 'ADD_LECTURE_TO_PLANNER';
+// export const REMOVE_LECTURE_FROM_PLANNER = BASE_STRING + 'REMOVE_LECTURE_FROM_PLANNER';
+export const REORDER_PLANNER = BASE_STRING + 'REORDER_PLANNER';
 export const UPDATE_CELL_SIZE = BASE_STRING + 'UPDATE_CELL_SIZE';
-export const SET_IS_DRAGGING = BASE_STRING + 'SET_IS_DRAGGING';
-export const SET_MOBILE_IS_PLANNER_TABS_OPEN = BASE_STRING + 'SET_MOBILE_IS_PLANNER_TABS_OPEN';
 /* eslint-enable prefer-template */
 
 export function reset() {
@@ -22,6 +20,7 @@ export function reset() {
   };
 }
 
+/*
 export function addLectureTopPlanner(lecture) {
   return {
     type: ADD_LECTURE_TO_PLANNER,
@@ -35,6 +34,7 @@ export function removeLectureFromPlanner(lecture) {
     lecture: lecture,
   };
 }
+*/
 
 export function setPlanners(planenrs) {
   return {
@@ -46,13 +46,6 @@ export function setPlanners(planenrs) {
 export function clearPlanners() {
   return {
     type: CLEAR_PLANNERS,
-  };
-}
-
-export function setMyPlannerLectures(lectures) {
-  return {
-    type: SET_MY_PLANNER_LECTURES,
-    lectures: lectures,
   };
 }
 
@@ -85,24 +78,18 @@ export function duplicatePlanner(id, planenr) {
   };
 }
 
+export function reorderPlanner(planner, arrangeOrder) {
+  return {
+    type: REORDER_PLANNER,
+    planner: planner,
+    arrangeOrder: arrangeOrder,
+  };
+}
+
 export function updateCellSize(width, height) {
   return {
     type: UPDATE_CELL_SIZE,
     width: width,
     height: height,
-  };
-}
-
-export function setIsDragging(isDragging) {
-  return {
-    type: SET_IS_DRAGGING,
-    isDragging: isDragging,
-  };
-}
-
-export function setMobileIsPlannerTabsOpen(mobileIsPlannerTabsOpen) {
-  return {
-    type: SET_MOBILE_IS_PLANNER_TABS_OPEN,
-    mobileIsPlannerTabsOpen: mobileIsPlannerTabsOpen,
   };
 }
---
+++ react/src/reducers/planner/planner.js
@@ -0,0 +1,158 @@
+import {
+  RESET,
+  SET_PLANNERS, CLEAR_PLANNERS,
+  SET_SELECTED_PLANNER,
+  CREATE_PLANNER, DELETE_PLANNER, DUPLICATE_PLANNER,
+  // ADD_LECTURE_TO_PLANNER, REMOVE_LECTURE_FROM_PLANNER,
+  REORDER_PLANNER,
+  UPDATE_CELL_SIZE,
+} from '../../actions/planner/planner';
+
+const initialState = {
+  planners: null,
+  selectedPlanner: null,
+  cellWidth: 200,
+  cellHeight: 50,
+  isDragging: false,
+  mobileIsPlannerTabsOpen: false,
+};
+
+const planner = (state = initialState, action) => {
+  switch (action.type) {
+    case RESET: {
+      return initialState;
+    }
+    case SET_PLANNERS: {
+      return Object.assign({}, state, {
+        planners: action.planners,
+        selectedPlanner: action.planners[0],
+      });
+    }
+    case CLEAR_PLANNERS: {
+      return Object.assign({}, state, {
+        planners: null,
+        selectedPlanner: null,
+      });
+    }
+    case SET_SELECTED_PLANNER: {
+      return Object.assign({}, state, {
+        selectedPlanner: action.planner,
+      });
+    }
+    case CREATE_PLANNER: {
+      const newArrangeOrder = state.planners.length > 0
+        ? Math.max(...state.planners.map((t) => t.arrange_order)) + 1
+        : 0;
+      const newPlanner = {
+        id: action.id,
+        lectures: [],
+        arrange_order: newArrangeOrder,
+      };
+      return Object.assign({}, state, {
+        selectedPlanner: newPlanner,
+        planners: [
+          ...state.planners,
+          newPlanner,
+        ],
+      });
+    }
+    case DELETE_PLANNER: {
+      const indexOfPlanner = state.planners.findIndex((t) => (t.id === action.planner.id));
+      const newPlanners = state.planners.filter((t) => (t.id !== action.planner.id));
+      const newSelectedPlanner = (indexOfPlanner !== state.planners.length - 1)
+        ? newPlanners[indexOfPlanner]
+        : newPlanners[indexOfPlanner - 1];
+      return Object.assign({}, state, {
+        selectedPlanner: newSelectedPlanner,
+        planners: newPlanners,
+      });
+    }
+    case DUPLICATE_PLANNER: {
+      const newPlanner = {
+        id: action.id,
+        lectures: action.planner.lectures.slice(),
+        arrange_order: Math.max(...state.planners.map((t) => t.arrange_order)) + 1,
+      };
+      return Object.assign({}, state, {
+        selectedPlanner: newPlanner,
+        planners: [
+          ...state.planners,
+          newPlanner,
+        ],
+      });
+    }
+    /*
+    case ADD_LECTURE_TO_PLANNER: {
+      const newPlanner = {
+        id: state.selectedPlanner.id,
+        lectures: state.selectedPlanner.lectures.concat([action.lecture]),
+      };
+      const newPlanners = state.planners.map((t) => (
+        t.id === newPlanner.id
+          ? newPlanner
+          : t
+      ));
+      return Object.assign({}, state, {
+        selectedPlanner: newPlanner,
+        planners: newPlanners,
+      });
+    }
+    case REMOVE_LECTURE_FROM_PLANNER: {
+      const newPlanner = {
+        id: state.selectedPlanner.id,
+        lectures: state.selectedPlanner.lectures.filter((l) => (l.id !== action.lecture.id)),
+      };
+      const newPlanners = state.planners.map((t) => (
+        t.id === newPlanner.id
+          ? newPlanner
+          : t
+      ));
+      return Object.assign({}, state, {
+        selectedPlanner: newPlanner,
+        planners: newPlanners,
+      });
+    }
+    */
+    case REORDER_PLANNER: {
+      const newPlanners = state.planners.map((t) => {
+        if (t.id === action.planner.id) {
+          return {
+            ...t,
+            arrange_order: action.arrangeOrder,
+          };
+        }
+        if (action.arrangeOrder <= t.arrange_order && t.arrange_order < action.planner.arrange_order) {
+          return {
+            ...t,
+            arrange_order: t.arrange_order + 1,
+          };
+        }
+        if (action.planner.arrange_order < t.arrange_order && t.arrange_order <= action.arrangeOrder) {
+          return {
+            ...t,
+            arrange_order: t.arrange_order - 1,
+          };
+        }
+        return t;
+      });
+      // eslint-disable-next-line fp/no-mutating-methods
+      newPlanners.sort((t1, t2) => (t1.arrange_order - t2.arrange_order));
+      const updatedPlanner = newPlanners.find((t) => (t.id === state.selectedPlanner.id));
+      return Object.assign({}, state, {
+        planners: newPlanners,
+        selectedPlanner: updatedPlanner,
+      });
+    }
+    case UPDATE_CELL_SIZE: {
+      return Object.assign({}, state, {
+        cellWidth: action.width,
+        cellHeight: action.height,
+      });
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default planner;
