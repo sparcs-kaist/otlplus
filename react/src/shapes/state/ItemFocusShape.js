592e6ac0d16269e883755b31a74ab883182ac26f
zealotzealot <hsh0908y@gmail.com>
2023-03-20 22:36:32 +0900

    Fix missing fields in ItemFocusShape

--- react/src/shapes/state/ItemFocusShape.js
+++ react/src/shapes/state/ItemFocusShape.js
@@ -2,12 +2,19 @@ import PropTypes from 'prop-types';
 
 import { ItemFocusFrom } from '../../reducers/planner/itemFocus';
 import courseShape from '../model/CourseShape';
+import futurePlannerItemShape from '../model/FuturePlannerItemShape';
+import genericPlannerItemShape from '../model/GenericPlannerItemShape';
 import lectureShape from '../model/LectureShape';
 import reviewShape from '../model/ReviewShape';
+import takenPlannerItemShape from '../model/TakenPlannerItemShape';
 
 
 const itemFocusShape = PropTypes.shape({
   from: PropTypes.oneOf(Object.values(ItemFocusFrom)).isRequired,
+  clicked: PropTypes.bool.isRequired,
+  item: PropTypes.oneOfType([
+    takenPlannerItemShape, futurePlannerItemShape, genericPlannerItemShape,
+  ]),
   course: courseShape,
   reviews: PropTypes.arrayOf(reviewShape),
   lectures: PropTypes.arrayOf(lectureShape),
